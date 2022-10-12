62e67bb52320328ba78bac7306695a095fbc0ead
eunho8713 <dmsgh3303@gmail.com>
2022-10-12 11:19:16 +0900

    feat: Node구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/LinkedList.swift
@@ -0,0 +1,8 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이은호 on 2022/10/12.
+//
+
+import Foundation
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/Node.swift
@@ -0,0 +1,20 @@
+//
+//  Node.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이은호 on 2022/10/12.
+//
+
+import Foundation
+
+class Node<T> {
+    var prev: Node?
+    var data: T?
+    var next: Node?
+    
+    init(data: T?, prev: Node? = nil, next: Node? = nil) {
+        self.prev = prev
+        self.data = data
+        self.next = next
+    }
+}
