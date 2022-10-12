02e752b18a3285088166848336b9dd6f023e2cba
moonsoungjin <seongjinmun74@gmail.com>
2022-10-12 10:44:59 +0900

    fix: Node 타입 설계

--- BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 class Node<T> {
+    var value: T
+    var next: Node?
     
+    init(value: T, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
 }
