814f5c2c8229d4925269966c793e6be3efee5355
dddowon <dowon4095@naver.com>
2022-10-12 11:58:59 +0900

    feat: LinkedList클래스 - append, removeFirst 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/LinkedList.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class LinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    
+    func append(data: T?) {
+        if head == nil || tail == nil {
+            head = Node.init(data: data)
+            tail = head
+            return
+        }
+        
+        let newNode = Node.init(data: data)
+        tail?.next = newNode
+        newNode.prev = tail
+        tail = newNode
+    }
+    
+    func removeFirst() {
+        if head == nil || tail == nil { return }
+        
+        if head?.next == nil {
+            head = nil
+            tail = nil
+            return
+        }
+        
+        head?.next?.prev = head?.prev
+        head = head?.next
+    }
+}
+
+
+
+
