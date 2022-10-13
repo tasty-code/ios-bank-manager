2755324a7a5f83adf64562aa22da7ee559f28228
wormsJJG <jjr66461837@gmail.com>
2022-10-13 10:20:31 +0900

    feat: 로직설계전 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
-
+print("1 : 은행개점")
+print("2 : 종료")
+print("입력 :", terminator: " ")
+let input = readLine()!
+let myung = Int.random(in: 10...30)
+var queue = Queue<Int>()
+var count = 0
+var time: Double = 0.0
+for i in 1...myung {
+    queue.enqueue(i)
+}
+for i in 1...myung {
+    print("\(i)번 고객 업무 시작")
+    queue.dequeue()
+    print("\(i)번 고객 업무 종료")
+    count += 1
+    time += 0.7
+}
+let str = String(format: "%.2f", time)
+print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 10명이며, 총 업무시간은 \(str)초 입니다.")
