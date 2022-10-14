6360bb524f62e3e91b8838c15b19a97d2dea1634
parkseyoon <issou06261@gmail.com>
2022-10-14 13:50:04 +0900

    feat: Queue 타입 선언

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -0,0 +1,16 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by 세윤 on 2022/10/14.
+//
+
+import Foundation
+
+class Queue {
+    var queue: LinkedList = LinkedList<Any>()
+    
+    func enqueue(item: Any) {
+        
+    }
+}
