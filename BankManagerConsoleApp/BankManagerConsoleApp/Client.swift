8bbe342758e0e7ccdf496f315ac9e6d16d96c26d
dddowon <dowon4095@naver.com>
2022-10-17 11:51:49 +0900

    feat: Clinet 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
@@ -2,7 +2,14 @@
 //  Client.swift
 //  BankManagerConsoleApp
 //
-//  Created by 이은호 on 2022/10/17.
+//  Created by 박도원 on 2022/10/17.
 //
 
 import Foundation
+
+struct Client {
+    func randomClientNumber() -> Int {
+        let clientNumber = Int.random(in: 10...30)
+        return clientNumber
+    }
+}
