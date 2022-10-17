f8e6478e40667bf8ce56addb29a9f829cdd1ba41
eunho8713 <dmsgh3303@gmail.com>
2022-10-17 11:03:45 +0900

    feat: 은행타입에 메뉴 받아오는 함수 구현

--- BankManager.swift
+++ BankManager.swift
@@ -6,3 +6,14 @@
 
 import Foundation
 
+func choiceMenu() {
+    print("1 : 은행 개점" )
+    print("2 : 종료")
+    print("입력 : ", terminator: "")
+    
+    guard let inputMenu = Int(readLine()!) else {
+        return
+    }
+}
+
+
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,8 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이은호 on 2022/10/17.
+//
+
+import Foundation
