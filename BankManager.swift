79d25ff3052e386b9f9f7bb2468fa60009b31cdd
dddowon <dowon4095@naver.com>
2022-10-18 16:06:20 +0900

    feat: 업무시간 구현

--- BankManager.swift
+++ BankManager.swift
@@ -10,6 +10,7 @@ struct BankManager {
     func work() {
         let clientNumber = Client().randomClientNumber()
         let clientList = Queue<Int>()
+        let workTime: Double = 0.7
         
         for i in 1...clientNumber {
             clientList.enqueue(data: i)
@@ -17,6 +18,10 @@ struct BankManager {
             print("\(client) 번 고객 업무 시작")
             print("\(client) 번 고객 업무 종료")
         }
+        let totalWorkTime: Double = workTime * Double(clientNumber)
+        let strTotalWorkTime = String(format: "%.2f", totalWorkTime)
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(clientNumber)명 이며, 총 업무시간은 \(strTotalWorkTime)초입니다.")
+        
     }
     
 }
