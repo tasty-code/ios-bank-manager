f3da9c8d77b643bf88949c1619497ce65c82ffc7
devxsby <devxsby@gmail.com>
2023-02-21 13:18:29 +0900

    Feat: Queue 구조체 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -0,0 +1,32 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by devxsby on 2023/02/21.
+//
+
+import Foundation
+
+struct Queue<Value: Equatable> {
+    private var queue: LinkedList = LinkedList<Value>()
+    
+    var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    mutating func euquueue(_ item: Value) {
+        queue.append(item)
+    }
+    
+    mutating func dequeue() -> Value? {
+        return queue.pop()
+    }
+    
+    mutating func peek() -> Value? {
+        return queue.first
+    }
+    
+    mutating func clear() {
+        queue.removeAll()
+    }
+}
