c7bec397101bc389b16d2131d887a2bf7ea8373e
Jason <ggj00125@gmail.com>
2023-02-21 13:58:03 +0900

    Feat: UnitTest Target 추가

---
+++ BankManagerConsoleApp/QueueTest/QueueTest.swift
@@ -0,0 +1,20 @@
+//
+//  QueueTest.swift
+//  QueueTest
+//
+//  Created by Jason on 2023/02/21.
+//
+
+import XCTest
+@testable import BankManagerConsoleApp
+
+final class QueueTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}
