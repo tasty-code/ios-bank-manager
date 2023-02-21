468c048c4933693ad4d25b4d56366c1f3c93316e
Lust3r <s1skyblue1s@gmail.com>
2023-02-21 13:17:49 +0900

    [feat] Linked-list 타입 구현 및 list에 사용할 Node class 정의

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,53 @@
 //
 
 import Foundation
+
+final class Node<T> {
+
+    var data: T
+    var next: Node?
+
+    init(data: T, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+struct LinkedList<T> {
+
+    var head: Node<T>?
+    var tail: Node<T>?
+    var count = 0
+
+    mutating func append(_ data: T) {
+        let newNode = Node(data: data)
+
+        count += 1
+
+        if head == nil {
+            head = newNode
+            tail = head
+            return
+        }
+
+        tail?.next = newNode
+        tail = newNode
+    }
+
+    mutating func removeFirst() -> T? {
+        guard count != 0 else { return nil }
+
+        let removedValue = head?.data
+
+        head = head?.next
+
+        if head == nil {
+            tail = nil
+            count = 0
+        } else {
+            count -= 1
+        }
+
+        return removedValue
+    }
+}
