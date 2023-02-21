e32f2eaec31291ec08f9d42c91a12971f010a95c
Jason <ggj00125@gmail.com>
2023-02-21 14:18:15 +0900

    Feat: UnitTest isEmpty메서드 및 dequeue메서드 테스트 추가 후 통과 확인

--- BankManagerConsoleApp/QueueTest/QueueTest.swift
+++ BankManagerConsoleApp/QueueTest/QueueTest.swift
@@ -34,4 +34,29 @@ final class QueueTest: XCTestCase {
         //then
         XCTAssertEqual(actualCountNumber, givenCountNumber)
     }
+    
+    func test_3을_enque했을때_Queue안에_Node가_존재하는지() {
+        //given
+        
+        //when
+        sut.euqueue(3)
+        
+        //then
+        XCTAssertFalse(sut.isEmpty)
+    }
+    
+    func test_enque메서드_호출뒤_deque했을때_카운트가_0개인지() {
+        //given
+        let givenCountNumber = 0
+        
+        //when
+        sut.euqueue(3)
+        sut.euqueue(4)
+        
+        let _ = sut.dequeue()
+        let _ = sut.dequeue()
+        let actualCountNumber = sut.count
+        //then
+        XCTAssertEqual(actualCountNumber, givenCountNumber)
+    }
 }
