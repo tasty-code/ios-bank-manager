dd38860b394e5575bf91e6834cbd174e3d18a632
Lust3r <s1skyblue1s@gmail.com>
2023-02-22 16:58:27 +0900

    [feat] Customer 타입 및 관련 프로토콜 구현
    - Waitable 프로토콜 요구사항: 고객의 대기번호(waitingOrder)
    - Customer는 Waitable 프로토콜을 채택

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이상윤 on 2023/02/22.
+//
+
+import Foundation
+
+struct Customer: Waitable {
+    var waitingOrder: Int
+
+}
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Protocol/Waitable.swift
@@ -0,0 +1,12 @@
+//
+//  Waitable.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이상윤 on 2023/02/22.
+//
+
+import Foundation
+
+protocol Waitable {
+    var waitingOrder: Int { get }
+}
