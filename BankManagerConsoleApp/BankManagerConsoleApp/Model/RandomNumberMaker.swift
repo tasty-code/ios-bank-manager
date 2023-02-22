5b079f3de19b075967d50701d46697bd6aca02c1
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-02-22 17:15:09 +0900

    [feat] 임의의 수를 생성하는 RandomNumberMaker 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/RandomNumberMaker.swift
@@ -0,0 +1,13 @@
+//
+//  RandomNumberMaker.swift
+//  BankManagerConsoleApp
+//
+//  Created by Blu on 2023/02/22.
+//
+
+import Foundation
+
+struct RandomNumberMaker {
+
+    func makeRandomNumber() -> Int { Int.random(in: (10...30)) }
+}
