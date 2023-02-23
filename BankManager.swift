e1708e1cba30908ea489738dff81fc1f4744d87e
Jason <ggj00125@gmail.com>
2023-02-23 16:11:27 +0900

    Feat: BankManager 메서드 구현
    - beginWork()
    - calculate(spend totalCount: )

--- BankManager.swift
+++ BankManager.swift
@@ -15,6 +15,17 @@ enum WorkList: String, CustomStringConvertible {
     }
 }
 
+enum Requirement {
+    
+    enum CustomerCount {
+        static let defaultCustomer: Int = 1
+        static let minimum: Int = 10
+        static let maxmimum: Int = 30
+    }
+    
+    static let leadTime: Double = 0.7
+}
+
 struct BankManager {
     static func startProcess() {
         var isContinue = true
@@ -23,18 +34,49 @@ struct BankManager {
             print(OutputMessage.programStart)
             
             guard let userInput = readLine() else {
+                //TODO: Error Handling
                 return
             }
             
             switch WorkList(rawValue: userInput) {
             case .openBank:
-                return
+                OutputMessage.conform(userInput)
+                return beginWork()
             case .closeBank:
-                OutputMessage.exitProgram(userInput)
+                OutputMessage.conform(userInput)
                 isContinue = false
             default:
                 return
             }
         } while isContinue == true
+    }
+    
+    private static func beginWork() {
+        let teller = Teller(identifier: "Teller1")
+        let totalCustomCount = Int.random(in: Requirement.CustomerCount.minimum...Requirement.CustomerCount.maxmimum)
+        
+        for customNumber in Requirement.CustomerCount.defaultCustomer..<totalCustomCount {
+            OutputMessage.work(start: customNumber)
+
+            OutputMessage.work(finish: customNumber)
+        }
+        let totalSpend = calculate(spend: totalCustomCount)
+
+        OutputMessage.todayWorkDeadline(customer: totalCustomCount, leadTime: totalSpend)
+    }
+    
+    private static func calculate(spend toalCount: Int) -> String {
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.roundingMode = .floor
+        numberFormatter.maximumSignificantDigits = 4
+        
+        let totalLeadTime = Requirement.leadTime * Double(toalCount)
+        
+        guard let totalSpend = numberFormatter.string(for: totalLeadTime) else {
+            return ""
+        }
+        
+        return totalSpend
     }
 }
