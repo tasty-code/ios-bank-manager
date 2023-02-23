f354a477089494efc07c5eed6af2c7a2fe4582a6
Jason <ggj00125@gmail.com>
2023-02-23 14:02:19 +0900

    Feat: BankMananger 구현
    - 타입 메서드 startProcess()
    - 열거형 타입 WorkList

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,36 @@
 //
 
 import Foundation
+
+enum WorkList: String, CustomStringConvertible {
+    case openBank = "1"
+    case closeBank = "2"
+    
+    var description: String {
+        return self.rawValue
+    }
+}
+
+struct BankManager {
+    static func startProcess() {
+        var isContinue = true
+        
+        repeat {
+            print(OutputMessage.programStart)
+            
+            guard let userInput = readLine() else {
+                return
+            }
+            switch WorkList(rawValue: userInput) {
+            case .openBank:
+                return
+            case .closeBank:
+                OutputMessage.exitProgram(userInput)
+                isContinue = false
+            default:
+                return
+            }
+            
+        } while isContinue == true
+    }
+}
