d862d1e8c104b70c3f913e75fb3e535a95e118a6
Jason <ggj00125@gmail.com>
2023-02-23 14:58:21 +0900

    Chore: 업무 마감 메서드명 변경
    - exitProgram 메서드명이 업무 마감과 의미가 어울리지않음.

--- BankManagerConsoleApp/BankManagerConsoleApp/View/OutputMessage.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/View/OutputMessage.swift
@@ -18,7 +18,7 @@ enum OutputMessage: String, CustomStringConvertible {
         print("\(customNumber)번 고객 업무 완료")
     }
     
-    static func exitProgram(customer: Int, leadTime: Int) {
+    static func todayWorkDeadline(customer: Int, leadTime: Int) {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customer)명이며, 총 업무시간은 \(leadTime)초입니다.")
     }
     
