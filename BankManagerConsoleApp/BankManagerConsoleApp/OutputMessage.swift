0ad00e8199d3b63032aa3d05837f4d30c32f2898
Jason <ggj00125@gmail.com>
2023-02-23 13:45:07 +0900

    Feat: OutputMessage 타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/OutputMessage.swift
@@ -0,0 +1,28 @@
+//
+//  OutputMessage.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jason on 2023/02/23.
+//
+
+import Foundation
+
+enum OutputMessage: String, CustomStringConvertible {
+    case programStart = "1 : 은행 개점 \n2 : 종료"
+    
+    static func work(start waitNumber: Int) {
+        print("\(waitNumber)번 고객 업무 시작")
+    }
+    
+    static func work(finish customNumber: Int) {
+        print("\(customNumber)번 고객 업무 완료")
+    }
+    
+    static func exitProgram(customer: Int, leadTime: Int) {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customer)명이며, 총 업무시간은 \(leadTime)초입니다.")
+    }
+    
+    var description: String {
+        return self.rawValue
+    }
+}
