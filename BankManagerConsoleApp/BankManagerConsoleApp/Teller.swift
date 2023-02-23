028f5245c979e6abeea74f8094e9ca3712ebe8be
Jason <ggj00125@gmail.com>
2023-02-23 16:11:39 +0900

    Feat: Teller 구조체 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Teller.swift
@@ -0,0 +1,15 @@
+//
+//  Teller.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jason on 2023/02/23.
+//
+
+import Foundation
+
+struct Teller: Workable {
+    var identifier: String
+    private let customerLinkedList = LinkedList<Int>()
+    
+    
+}
