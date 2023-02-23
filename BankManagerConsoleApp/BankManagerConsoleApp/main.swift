f9e55ce58c925e3a60b86046c08a9e4bc873d3ea
ohdair <ohdiar@gmail.com>
2023-02-23 13:46:50 +0900

    feat: 프로그램 실행시 console에 두 개의 메뉴를 출력하는 함수 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,17 @@
 // 
 
 import Foundation
+
+
+while true {
+    bankOpener()
+    if readLine() == "2" {
+        break
+    }
+}
+
+func bankOpener() {
+    print("1 : 은행개점")
+    print("2 : 종료")
+    print("입력 : ", terminator: "")
+}
