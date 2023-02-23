38ebeb851f38f607e469816500afb43262bbd198
iyeahh <yboravender@gmail.com>
2023-02-23 15:15:40 +0900

    feat: 은행개점과 종료를 입력받을 수 있는 execute 함수 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,23 @@
 // 
 
 import Foundation
+
+func execute() {
+    print("1 : 은행개점")
+    print("2 : 종료")
+    print("입력 : ", terminator: "")
+    let input = readLine() ?? ""
+
+    switch input {
+    case "1":
+        print("은행개점")
+        execute()
+    case "2":
+        print("종료")
+    default:
+        print("입력이 잘못되었습니다.")
+        execute()
+    }
+}
+
+execute()
