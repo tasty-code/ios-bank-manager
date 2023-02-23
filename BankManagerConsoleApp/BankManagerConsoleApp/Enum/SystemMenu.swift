dd24c5fbfcfa663d546e78419277eb6c6edb6655
Lust3r <s1skyblue1s@gmail.com>
2023-02-23 15:39:25 +0900

    [feat] 메뉴를 선택했을 때 문구를 출력할 SystemMenu enum 타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Enum/SystemMenu.swift
@@ -0,0 +1,22 @@
+//
+//  SystemMenu.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이상윤 on 2023/02/23.
+//
+
+import Foundation
+
+enum SystemMenu: Int, CaseIterable, CustomStringConvertible {
+    case open = 1
+    case close = 2
+
+    var description: String {
+        switch self {
+        case .open:
+            return "은행개점"
+        case .close:
+            return "종료"
+        }
+    }
+}
