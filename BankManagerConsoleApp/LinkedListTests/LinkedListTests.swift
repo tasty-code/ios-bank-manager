66edddf8c19eeb200cef7d7d17ca24890d636d6a
iyeahh <yboravender@gmail.com>
2023-02-24 10:31:08 +0900

    refactor: test 코드의 상수명 value -> given으로 변경

--- BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
+++ BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
@@ -30,24 +30,24 @@ final class LinkedListTests: XCTestCase {
     }
 
     func test_처음_append한_head의_값이_headValue가_된다() {
-        let value = "iYeah"
-        sut.append(value)
+        let given = "iYeah"
+        sut.append(given)
 
         let expectation = sut.headValue
-        XCTAssertEqual(value, expectation)
+        XCTAssertEqual(given, expectation)
     }
 
     func test_removeFirst시_첫번째_값이_반환된다() {
-        let value = "iYeah"
-        sut.append(value)
+        let given = "iYeah"
+        sut.append(given)
 
         let expectation = sut.removeFirst()
-        XCTAssertEqual(value, expectation)
+        XCTAssertEqual(given, expectation)
     }
 
     func test_하나의_값을_넣고_removeFirst시_headValue는_nil이다() {
-        let value = "iYeah"
-        sut.append(value)
+        let given = "iYeah"
+        sut.append(given)
         sut.removeFirst()
 
         let expectation = sut.headValue
@@ -55,30 +55,30 @@ final class LinkedListTests: XCTestCase {
     }
 
     func test_removeFirst시_먼저_넣은_값의_순서대로_반환된다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.append($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
 
-        values.forEach { value in
+        givens.forEach { given in
             let expectation = sut.removeFirst()
-            XCTAssertEqual(value, expectation)
+            XCTAssertEqual(given, expectation)
         }
     }
 
     func test_removeFirst시_headValue가_다음값으로_변경된다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.append($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
 
-        values.forEach { value in
+        givens.forEach { given in
             let expectation = sut.headValue
-            XCTAssertEqual(value, expectation)
+            XCTAssertEqual(given, expectation)
 
             sut.removeFirst()
         }
     }
 
     func test_removeAll시_isEmpty가_true이다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.append($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
 
         sut.removeAll()
         let expectation = sut.isEmpty
@@ -86,8 +86,8 @@ final class LinkedListTests: XCTestCase {
     }
 
     func test_removeAll시_headValue이_nil이다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.append($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
 
         sut.removeAll()
         let expectation = sut.headValue
--- BankManagerConsoleApp/QueueTests/QueueTests.swift
+++ BankManagerConsoleApp/QueueTests/QueueTests.swift
@@ -30,24 +30,24 @@ final class QueueTests: XCTestCase {
     }
 
     func test_처음_enqueue한_head의_값이_peek가_된다() {
-        let value = "iYeah"
-        sut.enqueue(value)
+        let given = "iYeah"
+        sut.enqueue(given)
 
         let expectation = sut.peek
-        XCTAssertEqual(value, expectation)
+        XCTAssertEqual(given, expectation)
     }
 
     func test_dequeue시_첫번째_값이_반환된다() {
-        let value = "iYeah"
-        sut.enqueue(value)
+        let given = "iYeah"
+        sut.enqueue(given)
 
         let expectation = sut.dequeue()
-        XCTAssertEqual(value, expectation)
+        XCTAssertEqual(given, expectation)
     }
 
     func test_하나의_값을_넣고_dequeue시_peek는_nil이다() {
-        let value = "iYeah"
-        sut.enqueue(value)
+        let given = "iYeah"
+        sut.enqueue(given)
         sut.dequeue()
 
         let expectation = sut.peek
@@ -55,30 +55,30 @@ final class QueueTests: XCTestCase {
     }
 
     func test_dequeue시_먼저_넣은_값의_순서대로_반환된다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.enqueue($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
 
-        values.forEach { value in
+        givens.forEach { given in
             let expectation = sut.dequeue()
-            XCTAssertEqual(value, expectation)
+            XCTAssertEqual(given, expectation)
         }
     }
 
     func test_dequeue시_peek가_다음값으로_변경된다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.enqueue($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
 
-        values.forEach { value in
+        givens.forEach { given in
             let expectation = sut.peek
-            XCTAssertEqual(value, expectation)
+            XCTAssertEqual(given, expectation)
 
             sut.dequeue()
         }
     }
 
     func test_clear시_isEmpty가_true이다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.enqueue($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
 
         sut.clear()
         let expectation = sut.isEmpty
@@ -86,8 +86,8 @@ final class QueueTests: XCTestCase {
     }
 
     func test_clear시_peek가_nil이다() {
-        let values = ["iYeah", "Mason", "vivi"]
-        values.forEach { sut.enqueue($0) }
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
 
         sut.clear()
         let expectation = sut.peek
