f4590331a4b3acf709e6fe203373afe4ccd55b5d
SunnnySong <tjsghktjswls@naver.com>
2023-02-27 01:06:15 +0900

    feat: Double extension 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Extension/DoubleExtension.swift
@@ -0,0 +1,15 @@
+//
+//  DoubleExtension.swift
+//  BankManagerConsoleApp
+//
+//  Created by 송선진 on 2023/02/27.
+//
+
+import Foundation
+
+extension Double {
+    func sleep() {
+        let usecondsTime = useconds_t(self * 1000000)
+        usleep(usecondsTime)
+    }
+}
