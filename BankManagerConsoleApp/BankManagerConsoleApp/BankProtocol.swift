5ce66f1e59e7c552b178af76348f69a3de48bbf1
sloDevz <onec555@gmail.com>
2023-02-27 01:03:05 +0900

    feat: BankProtocol, Customer 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  BankProtocol.swift
+//  BankManagerConsoleApp
+//
+//  Created by DONGWOOK SEO on 2023/02/27.
+//
+
+import Foundation
+
+typealias waitingNumber = UInt
+typealias customerInfo = (number: waitingNumber, customer: Customer)
+
+protocol BankProtocol {
+
+    func open()
+    
+    func close()
+}
