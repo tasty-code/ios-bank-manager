b8603dfb9d9c7a44eeb5e1a412af07e601c7f358
Mason Kim <qwerty3345@naver.com>
2023-02-27 15:50:32 +0900

    test: LinkedList, Queue의 count에 대한 테스트 케이스 추가

--- BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
+++ BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
@@ -93,4 +93,35 @@ final class LinkedListTests: XCTestCase {
         let expectation = sut.headValue
         XCTAssertNil(expectation)
     }
+
+    func test_아무것도_추가하지_않았을때_count가_0이다() {
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 0)
+    }
+
+    func test_3개를_추가했을때_count가_3이다() {
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
+
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 3)
+    }
+
+    func test_3개를_추가한뒤_1개를_제거하면_count가_2이다() {
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
+
+        sut.removeFirst()
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 2)
+    }
+
+    func test_removeAll시_count가_0이다() {
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.append($0) }
+
+        sut.removeAll()
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 0)
+    }
 }
--- BankManagerConsoleApp/QueueTests/QueueTests.swift
+++ BankManagerConsoleApp/QueueTests/QueueTests.swift
@@ -93,4 +93,35 @@ final class QueueTests: XCTestCase {
         let expectation = sut.peek
         XCTAssertNil(expectation)
     }
+
+    func test_아무것도_추가하지_않았을때_count가_0이다() {
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 0)
+    }
+
+    func test_3개를_추가했을때_count가_3이다() {
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
+
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 3)
+    }
+
+    func test_3개를_추가한뒤_1개를_제거하면_count가_2이다() {
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
+
+        sut.dequeue()
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 2)
+    }
+
+    func test_clear시_count가_0이다() {
+        let givens = ["iYeah", "Mason", "vivi"]
+        givens.forEach { sut.enqueue($0) }
+
+        sut.clear()
+        let expectation = sut.count
+        XCTAssertEqual(expectation, 0)
+    }
 }
