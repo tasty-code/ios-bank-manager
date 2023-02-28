56ec2fcd860949326295632885a3f5cc5b0f3d41
sloDevz <onec555@gmail.com>
2023-02-28 22:59:58 +0900

    feat: BankAbility 구조 개선

--- BankManagerConsoleApp/BankManagerConsoleApp/BankSystem/BankTextCollection.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankSystem/BankTextCollection.swift
@@ -15,7 +15,8 @@ enum BankTextCollection {
     }
     
     static func close(numberOfCustomer: UInt, customerTime: Double) -> String {
-        return "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무시간은 \(customerTime)초 입니다.\n"
+        let time = String(format:"%.2f", customerTime)
+        return "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무시간은 \(time)초 입니다.\n"
     }
     
     static func working(order: UInt, inProgress: Bool) -> String {
