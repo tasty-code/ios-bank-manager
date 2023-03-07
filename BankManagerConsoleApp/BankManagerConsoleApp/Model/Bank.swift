24e780f02b8c3f32db7f6a529532272035cd690f
Lust3r <s1skyblue1s@gmail.com>
2023-03-07 14:12:07 +0900

    [feat] DispatchQueue를 사용하여 비동기로 Customer 업무 처리
    - Semaphore를 사용하여 각 업무별 은행원 스레드 제한
    - group을 사용하여 모든 은행원이 업무가 끝난 이후에 메세지가 출력되도록 설정
    - calculateTotalWorkTime() 메서드를 통해 각 업무별 customer 수와 시간을 합한 값 반환

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -11,20 +11,43 @@ struct Bank: CustomerManageable {
     
     let accountBanker = Banker(processingTime: 0.7)
     let loanBanker = Banker(processingTime: 1.1)
-    let processingTimePerPerson = Banker(processingTime: 0.7).processingTime
 
     func open() {
         let numberOfTodayCustomers = makeAcceptableNumber()
         let customerQueue = makeCustomerQueue(maxCount: numberOfTodayCustomers)
+        let loanSemaphore = DispatchSemaphore(value: 1)
+        let accountSemaphore = DispatchSemaphore(value: 2)
+        let group = DispatchGroup()
+        var numberOfAccountCustomers = 0
+        var numberOfLoanCustomers = 0
 
         while !customerQueue.isEmpty() {
-            let currentCustomer = customerQueue.dequeue()
-            guard let numberOfCurrentCustomer = currentCustomer?.waitingOrder else { return }
+            guard let currentCustomer = customerQueue.dequeue() else { return }
 
-            accountBanker.work(of: numberOfCurrentCustomer, for: "account")
+            var currentCustomerWorkType = currentCustomer.workType
+
+            if currentCustomerWorkType == WorkList.account.rawValue {
+                DispatchQueue.global().async(group: group) {
+                    accountSemaphore.wait()
+                    accountBanker.work(of: currentCustomer.waitingOrder, for: currentCustomerWorkType)
+                    numberOfAccountCustomers += 1
+                    accountSemaphore.signal()
+                }
+            } else {
+                DispatchQueue.global().async(group: group) {
+                    loanSemaphore.wait()
+                    accountBanker.work(of: currentCustomer.waitingOrder, for: currentCustomerWorkType)
+                    numberOfLoanCustomers += 1
+                    loanSemaphore.signal()
+                }
+            }
         }
 
-        showWorkFinishMessage(numberOfTodayCustomers, processingTimePerPerson)
+        group.wait()
+
+        let totalWorkTime = calculateTotalWorkTime(customers: numberOfAccountCustomers, numberOfLoanCustomers, processingTimes: accountBanker.processingTime, loanBanker.processingTime)
+
+        showWorkFinishMessage(numberOfTodayCustomers, totalWorkTime)
     }
 
     func showWorkFinishMessage(_ totalNumber: Int, _ eachProcessingSecond: Double) {
@@ -33,4 +56,16 @@ struct Bank: CustomerManageable {
 
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalNumber)명이며, 총 업무시간은 \(convertDoubleToString)초입니다.")
     }
+
+    func calculateTotalWorkTime(customers: Int..., processingTimes: Double...) -> Double {
+        var result = 0.0
+
+        customers.enumerated().forEach {
+            let partialResult = Double($1) * processingTimes[$0]
+            result += partialResult
+        }
+
+        return result
+    }
+
 }
