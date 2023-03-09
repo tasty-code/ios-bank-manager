07f29ddf305a7536dc7386d6ee806280969ac68f
Mason Kim <qwerty3345@naver.com>
2023-03-09 20:21:47 +0900

    design: UI 헤더 부분 구성

--- BankManagerUIApp/BankManagerUIApp/Controller/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/Controller/ViewController.swift
@@ -6,14 +6,116 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    private enum Constants {
+        static let addCustomerButtonTitle = "고객 10명 추가"
+        static let resetButtonTitle = "초기화"
+        static let workTimeLabelFormText = "업무시간 - "
+        static let waitingStateLabelText = "대기중"
+        static let workingStateLabelText = "업무중"
+
+    }
+
+    // MARK: - UI Properties
+
+    private let addCustomerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(Constants.addCustomerButtonTitle, for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        return button
+    }()
+
+    private let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(Constants.resetButtonTitle, for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        return button
+    }()
+
+    private lazy var buttonStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [addCustomerButton, resetButton])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private let workTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.workTimeLabelFormText
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .title3)
+        return label
+    }()
+
+    private let waitingStateLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.waitingStateLabelText
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.backgroundColor = .systemGreen
+        return label
+    }()
+
+    private let workingStateLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.workingStateLabelText
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.backgroundColor = .systemIndigo
+        return label
+    }()
+
+    private lazy var stateLabelStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [waitingStateLabel, workingStateLabel])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private let waitingStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private let workingStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemBlue
+
+        view.backgroundColor = .white
+        configureLayout()
     }
 
+    // MARK: - Layout
+    private func configureLayout() {
+        let headerStackView = UIStackView(arrangedSubviews: [
+            buttonStackView,
+            workTimeLabel,
+            stateLabelStackView
+        ])
+        headerStackView.axis = .vertical
+        headerStackView.distribution = .fillEqually
 
+        view.addSubview(headerStackView)
+        headerStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerStackView.heightAnchor.constraint(equalToConstant: 150),
+            headerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
