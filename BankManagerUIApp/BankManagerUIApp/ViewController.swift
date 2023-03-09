e63002dc262883035655dd3da3ca42d4fbeb19d6
sloDevz <onec555@gmail.com>
2023-03-09 16:20:38 +0900

    feat: 각 요소들 StackView로 묶음

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -30,8 +30,8 @@ class ViewController: UIViewController {
        let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
-        stackView.alignment = .center
-        stackView.spacing = 5
+        stackView.alignment = .fill
+        stackView.spacing = 0
 
         return stackView
     }()
@@ -41,12 +41,14 @@ class ViewController: UIViewController {
     private let taskLabel: UILabel = {
         let label = UILabel()
         label.text = "업무시간 -"
+        label.textAlignment = .right
         label.font = UIFont.systemFont(ofSize: 20)
         return label
     }()
     
     private let timerLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .left
         label.font = UIFont.systemFont(ofSize: 20)
         label.text = "00:00:000"
         return label
@@ -90,29 +92,51 @@ class ViewController: UIViewController {
        let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
-        stackView.alignment = .center
+        stackView.alignment = .fill
         stackView.spacing = 0
+        
+        return stackView
+    }()
+    
+    //MARK: - Properties: Interface Stack
+    
+    private let interfaceStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
         
         return stackView
     }()
     
     //MARK: - Properties: CustomerWaiting&Inprogress Stack
-    
-    private let customerWaitingStackView: UIStackView = {
+    private let waitingInprogressStackView: UIStackView = {
        let stackView = UIStackView()
-        stackView.axis = .vertical
+        stackView.axis = .horizontal
         stackView.distribution = .fillEqually
-        stackView.alignment = .center
+        stackView.alignment = .fill
         stackView.spacing = 0
         
         return stackView
     }()
     
-    private let customerInprogressStackView: UIStackView = {
+    
+    private let waitingStackView: UIStackView = {
        let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
-        stackView.alignment = .center
+        stackView.alignment = .fill
+        stackView.spacing = 0
+        
+        return stackView
+    }()
+    
+    private let inprogressStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
         stackView.spacing = 0
         
         return stackView
@@ -123,63 +147,80 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        setButtonStack()
-        setTimerLabelStackView()
-        setProgressStackView()
+
     }
     
     //MARK: - UI Setting
     
     private func setUI() {
-        view.addSubview(addCustomersButton)
-        view.addSubview(resetButton)
-        view.addSubview(buttonStackView)
+        view.addSubview(interfaceStackView)
+        view.addSubview(waitingInprogressStackView)
         
-        view.addSubview(timerLabel)
-        view.addSubview(taskLabel)
-        view.addSubview(timerLabelStackView)
-        
-        view.addSubview(standbyLabel)
-        view.addSubview(inprogressLabel)
-        view.addSubview(progressStackView)
+        setButtonStack()
+        setTimerLabelStackView()
+        setProgressStackView()
+        setInterfaceStackView()
+        setCustomerWaitingStack()
+        setCustomerInprogressStack()
+        setWaitingAndInprogressStack()
     }
     
     private func setButtonStack() {
         buttonStackView.addArrangedSubview(addCustomersButton)
         buttonStackView.addArrangedSubview(resetButton)
         
-        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
-        buttonStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 0).isActive = true
-        buttonStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
-        buttonStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
     }
     
     private func setTimerLabelStackView() {
         timerLabelStackView.addArrangedSubview(taskLabel)
         timerLabelStackView.addArrangedSubview(timerLabel)
-        
-        timerLabelStackView.translatesAutoresizingMaskIntoConstraints = false
-        timerLabelStackView.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor,constant: 10).isActive = true
-        timerLabelStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+
     }
     
     private func setProgressStackView() {
         progressStackView.addArrangedSubview(standbyLabel)
         progressStackView.addArrangedSubview(inprogressLabel)
+    }
+    
+    private func setInterfaceStackView() {
+        interfaceStackView.addArrangedSubview(buttonStackView)
+        interfaceStackView.addArrangedSubview(timerLabelStackView)
+        interfaceStackView.addArrangedSubview(progressStackView)
         
-        progressStackView.translatesAutoresizingMaskIntoConstraints = false
-        progressStackView.topAnchor.constraint(equalTo: timerLabelStackView.bottomAnchor,constant: 15).isActive = true
-        progressStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
-        progressStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
-        progressStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        interfaceStackView.translatesAutoresizingMaskIntoConstraints = false
+        interfaceStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        interfaceStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        interfaceStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
     }
     
     private func setCustomerWaitingStack() {
-        customerWaitingStackView.addArrangedSubview(CustomerInfoView())
+        let customer: CustomerInfoView = {
+            let label = CustomerInfoView(frame: .zero)
+            label.taskTypeLabel.font = UIFont.systemFont(ofSize: 30)
+            
+            return label
+        }()
+        waitingStackView.addArrangedSubview(customer)
     }
     
     private func setCustomerInprogressStack() {
-        customerInprogressStackView.addArrangedSubview(CustomerInfoView())
+        let customer: CustomerInfoView = {
+            let label = CustomerInfoView(frame: .zero)
+            label.taskTypeLabel.font = UIFont.systemFont(ofSize: 30)
+            
+            return label
+        }()
+        waitingStackView.addArrangedSubview(customer)
+    }
+    
+    private func setWaitingAndInprogressStack() {
+        waitingInprogressStackView.addArrangedSubview(waitingStackView)
+        waitingInprogressStackView.addArrangedSubview(inprogressStackView)
+        
+        waitingInprogressStackView.translatesAutoresizingMaskIntoConstraints = false
+        waitingInprogressStackView.topAnchor.constraint(equalTo: interfaceStackView.topAnchor, constant: 0).isActive = true
+        waitingInprogressStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        waitingInprogressStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
     }
     
     //MARK: - UI Action
