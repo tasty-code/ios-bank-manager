91b7cb45bf2e4dfe3c55fd3f4c41ae53ac25264c
YunSwain <destap@naver.com>
2023-10-30 13:36:23 +0900

    feat: 자료구조 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/DataStructure/Queue.swift
@@ -0,0 +1,36 @@
+protocol QueueProtocol {
+    associatedtype Element
+    
+    var isEmpty: Bool { get }
+    
+    mutating func enqueue(_ element: Element)
+    mutating func dequeue() -> Element?
+    mutating func clear()
+    func peek() -> Element?
+}
+
+final class Queue<Element>: QueueProtocol {
+    typealias Element = Element
+    
+    private var container = LinkedList<Element>()
+    
+    var isEmpty: Bool {
+        return self.container.isEmpty
+    }
+    
+    func enqueue(_ element: Element) {
+        self.container.enqueue(element)
+    }
+    
+    func dequeue() -> Element? {
+        return self.container.dequeue()
+    }
+    
+    func clear() {
+        self.container.clear()
+    }
+    
+    func peek() -> Element? {
+        self.container.peek()
+    }
+}
