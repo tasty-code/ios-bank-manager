10583c8946c139450acdc725466d155a20b3388d
JJong <jfknfj920@naver.com>
2023-10-30 15:22:38 +0900

    feat: queue 프로토타입 추가

--- BankManager.swift
+++ BankManager.swift
@@ -1,7 +1,39 @@
-//
-//  BankManager.swift
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
+
+final class Queue<T> {
+    private var head: Person<T>?;
+    
+    init(head: Person<T>) {
+        self.head = head
+    }
+    
+    func enqueue(person: Person<T>) {
+//        if peek()
+    }
+    
+    func dequeue() {
+ 
+    }
+    
+    func clear() {
+        
+    }
+    
+    func peek() {
+        
+    }
+    
+    func isEmpty() {
+        
+    }
+}
+
+final class Person<T> {
+    let data: T
+    var next: Person?
+    
+    init(data: T, next: Person) {
+        self.data = data
+        self.next = next
+    }
+}
