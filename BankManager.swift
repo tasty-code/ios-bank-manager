12d5bbb5c43dab1b709e8034bd23b745adaf0efb
rarlala <wtw9611@gmail.com>
2023-10-31 11:09:55 +0900

    [chore] 주석 제거, final 키워드 추가

--- BankManager.swift
+++ BankManager.swift
@@ -1,9 +1,7 @@
 import Foundation
 
-class Node<T> {
-    // class 사용이유 node의 next가 다음 node의 주소를 가리킨다. 그래서 참조 타입을 사용하는 것
-    
-    var data: T // 데이터 타입이 옵셔널일 이유가 있을까?
+final class Node<T> {
+    var data: T
     var next: Node<T>?
     
     init(data: T, next: Node? = nil) {
@@ -16,61 +14,44 @@ class Node<T> {
     }
 }
 
-// LinkedList의 타입을 class? struct?
-class LinkedList<T> {
-    private var head: Node<T>? // 가장 첫 번째의 node를 가리킨다.
-    //tail을 만들까? 있으면 enqueue를 할때 while을 통해서 마지막까지 보고서 넣는 것이 아니라 그냥 tail의 바로 뒤에 추가가 가능하다.
-    
-    // 배열을 생성 안하는 이유: 배열을 사용하면 이름만 linkedList고 실제로는 배열이다.
+final class LinkedList<T> {
+    private var head: Node<T>?
     
     func enqueue(data: T) {
-        
-        if head == nil { // head가 없다면 아무런 값이 list에 없기 때문
+        if head == nil {
             head = Node(data: data)
             return
         }
         
         var node = head
         
-        while node?.next != nil { // 가장 마지막 node를 찾는 과정
+        while node?.next != nil {
             node = node?.next
-            
         }
         
         node?.next = Node(data: data)
-       
     }
     
     func dequeue() -> Node<T>? {
-        
         let outNode = head
-    
-        // 기존 head를 반환
-        // 기존 head에서 가르키는 node가 head가 된다.
         head = head?.next
-        outNode?.next = nil // nil을 꼭 넣어 줘야 하나>?
         return outNode
-    } // outNode를 삭제해줘야하는거 아닌가? : dequeue시 자동으로 해제가된다. deinit으로 확인이 가능하다.
+    }
     
     func clear() {
         head = nil
     }
-    // 참조를 안하기 때문에 자동으로 equeue를 했던 것들은 해제가 된다.
     
     func peek() -> T? {
-    
         return head?.data
-        
     }
     
     func isEmpthy() -> Bool {
-        
         if head == nil {
             return true
         } else {
             return false
         }
-        
     }
 
 }
--- BankManagerConsoleApp/BankManagerConsoleAppTest/BankManagerConsoleAppTest.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTest/BankManagerConsoleAppTest.swift
@@ -4,25 +4,20 @@ import XCTest
 final class BankManagerConsoleAppTest: XCTestCase {
     
     var sut: LinkedList<Int>!
-
+    
     override func setUpWithError() throws {
-       try super.setUpWithError()
+        try super.setUpWithError()
         sut = LinkedList()
     }
-
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-
     }
     
-    
-//MARK: dequeue 확인
+    //MARK: dequeue 확인
     
     func test_아무것도_enqueue를_안하고_dequeue를_하면_nil인가() {
-    
-       let result = sut.dequeue()
-
-            
+        let result = sut.dequeue()
         XCTAssertNil(result)
     }
     
@@ -39,50 +34,45 @@ final class BankManagerConsoleAppTest: XCTestCase {
         sut.enqueue(data: 1)
         sut.enqueue(data: 2)
         
-        let result = sut.dequeue()
+        let result = sut.dequeue()?.data
         
-        XCTAssertNotEqual(result?.data, 2)
+        XCTAssertNotEqual(result, 2)
     }
     
     func test_1_2_enqueue하고_dequeue하면_head가_2를_향하나() {
         sut.enqueue(data: 1)
         sut.enqueue(data: 2)
         
-        sut.dequeue()
-        let result = sut.dequeue()?.data
+        let _ = sut.dequeue()
+        let result = sut.peek()
         XCTAssertEqual(result, 2)
     }
     
-   
-    
-
-// MARK: peek확인
+    // MARK: enqueue, peek확인
     func test_enqueue_아무것도_안하고_peek로_값이_있는지_확인() throws {
         XCTAssertNil(sut.peek())
     }
     
     func test_1_enqueue_하고_peek로_값이_있는지_확인() throws {
-        
         sut.enqueue(data: 1)
         
         XCTAssertNotNil(sut.peek())
     }
     
     func test_1_enqueue_하고_peek로_값_확인() throws {
-        
         sut.enqueue(data: 1)
         
         XCTAssertEqual(sut.peek(), 1)
     }
     
-//MARK: clear
+    //MARK: clear
     
     func test_1_2_enqueue하고_안비어있다() {
         sut.enqueue(data: 1)
         sut.enqueue(data: 2)
         
-
         let result = sut.peek()
+        
         XCTAssertNotNil(result)
     }
     
@@ -90,25 +80,22 @@ final class BankManagerConsoleAppTest: XCTestCase {
         sut.enqueue(data: 1)
         sut.enqueue(data: 2)
         sut.enqueue(data: 3)
-        
         sut.clear()
+        
         let result = sut.peek()
+        
         XCTAssertNil(result)
     }
     
-    
-    
-    
-// MARK: isEmpthy?
+    // MARK: isEmpthy?
     func test_아무것도_추가안했을때_비어있을까() {
         XCTAssertTrue(sut.isEmpthy())
     }
     
     func test_1_추가했을때_안비어있나() {
-        
         sut.enqueue(data: 1)
         
         XCTAssertFalse(sut.isEmpthy())
     }
-
+    
 }
