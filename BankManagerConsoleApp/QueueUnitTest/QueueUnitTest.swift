d1ae967338dbc5fe270a5425bc68efc22c48fab4
kimjunsung <assd1108@naver.com>
2023-10-31 10:07:26 +0900

    test: Queue test case 수정

--- BankManagerConsoleApp/QueueUnitTest/QueueUnitTest.swift
+++ BankManagerConsoleApp/QueueUnitTest/QueueUnitTest.swift
@@ -18,22 +18,42 @@ final class QueueUnitTest: XCTestCase {
         queue = nil
     }
     
-    func test_Dequeue가_제대로_되는지() {
+    // MARK: - Dequeue
+    func test_큐가_비었을_때_nil을_리턴하는지() {
+        XCTAssertNil(queue.dequeue())
+    }
+    
+    func test_enqueue를_하고_dequeue를_했을_때_enqueue의_값을_리턴하는지() {
         // given
-        var array = [Int]()
-        for _ in 1...100 {
-            let number = Int.random(in: -1000...1000)
-            array.append(number)
+        let number = 10
+        
+        // when
+        queue.enqueue(number)
+        
+        // then
+        XCTAssertEqual(number, queue.dequeue())
+    }
+    
+    func test_배열을_enqueue하고_모두_dequeue를_한_값들이_배열과_일치하는지() {
+        // given
+        let array = [3, 54, 123,67, 28]
+        
+        // when
+        for number in array {
             queue.enqueue(number)
         }
         
-        // then
-        for number in array {
-            XCTAssertEqual(number, queue.dequeue())
+        var dequeueList = [Int?]()
+        while !queue.isEmpty {
+            dequeueList.append(queue.dequeue())
         }
+        
+        // then
+        XCTAssertEqual(array, dequeueList)
     }
     
-    func test_Peek이_제대로_되는지() {
+    // MARK: - Peek
+    func test_Peek이_큐의_첫번째_값을_반환하는지() {
         // given
         var array = [Int]()
         for _ in 0..<10 {
@@ -52,15 +72,13 @@ final class QueueUnitTest: XCTestCase {
         XCTAssertEqual(queue.peek, array[randomNumber])
     }
     
-    func test_빈_배열일_때_Peek이_제대로_되는지() {
+    func test_빈_배열일_때_Peek이_nil을_반환하는지() {
         // then
         XCTAssertNil(queue.peek)
     }
     
+    // MARK: - isEmpty
     func test_isEmpty가_제대로_되는지() {
-        // then
-        XCTAssertTrue(queue.isEmpty)
-        
         // given
         queue.enqueue(0)
         
@@ -68,7 +86,12 @@ final class QueueUnitTest: XCTestCase {
         XCTAssertFalse(queue.isEmpty)
     }
     
-    func test_Clear가_제대로_되는지() {
+    func test_빈_배열일_때_isEmpty가_False인지() {
+        XCTAssertTrue(queue.isEmpty)
+    }
+    
+    // MARK: - Clear
+    func test_Clear했을_때_isEmpty가_true인지() {
         // given
         for _ in 0..<Int.random(in: 1...10) {
             queue.enqueue(Int.random(in: 0...100))
@@ -79,40 +102,18 @@ final class QueueUnitTest: XCTestCase {
         
         // then
         XCTAssertTrue(queue.isEmpty)
-        XCTAssertNil(queue.peek)
     }
     
-    func test_Queue1() {
-        var q1 = Queue<Int>()
-        for _ in 0..<10_000_000 {
-            q1.enqueue(Int.random(in: -1000...1000))
-        }
-        for _ in 0..<400_000 {
-            let _ = q1.dequeue()
-        }
-        XCTAssertFalse(q1.isEmpty)
-    }
-    
-    func test_Queue_종합테스트() {
+    func test_Clear했을_때_peek이_nil인지() {
         // given
-        let array = [20, 1, 3, 45, 6, 7, 52, 43, 6, 756]
-        
-        // when
-        for number in array {
-            queue.enqueue(number)
+        for _ in 0..<Int.random(in: 1...10) {
+            queue.enqueue(Int.random(in: 0...100))
         }
-        
-        // then
-        XCTAssertFalse(queue.isEmpty)
-        XCTAssertEqual(queue.peek, array.first)
-        XCTAssertEqual(queue.dequeue(), array.first)
         
         // when
         queue.clear()
         
-        //then
-        XCTAssertTrue(queue.isEmpty)
-        XCTAssertNil(queue.dequeue())
+        // then
         XCTAssertNil(queue.peek)
     }
 }
