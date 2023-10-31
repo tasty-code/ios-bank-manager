59c394de9a4dc5d71ec12f61d35a5f6329f888f6
COMDORI <02145s1@gmail.com>
2023-10-31 10:38:43 +0900

    feat: 새로운 유닛 테스트 추가

--- BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
@@ -9,27 +9,33 @@ import XCTest
 @testable import BankManagerConsoleApp
 
 final class BankManagerConsoleAppTests: XCTestCase {
-    private var queue : Queue<Int>!
+    private var intQueue: Queue<Int>!
+    private var stringQueue: Queue<String>!
+    private var doubleQueue: Queue<Double>!
     
     override func setUpWithError() throws {
-        self.queue = Queue<Int>()
+        self.intQueue = Queue<Int>()
+        self.stringQueue = Queue<String>()
+        self.doubleQueue = Queue<Double>()
     }
     
     override func tearDownWithError() throws {
-        self.queue = nil
+        self.intQueue = nil
+        self.stringQueue = nil
+        self.doubleQueue = nil
     }
     
     func test_10추가하기() {
         let num = 10
-        queue.enqueue(num)
-        guard let result = queue.peek() else { return }
+        intQueue.enqueue(num)
+        guard let result = intQueue.peek() else { return }
         XCTAssertEqual(num, result)
     }
     
     func test_15빼보기() {
         let num = 15
-        queue.enqueue(num)
-        guard let result = queue.dequeue() else {return}
+        intQueue.enqueue(num)
+        guard let result = intQueue.dequeue() else {return}
         XCTAssertEqual(result, 15)
     }
     
@@ -37,22 +43,66 @@ final class BankManagerConsoleAppTests: XCTestCase {
         let array = [10, 20, 30]
         
         for item in array {
-            queue.enqueue(item)
+            intQueue.enqueue(item)
         }
-        let result = queue.check()
+        let result = intQueue.check()
         print(result)
         XCTAssertEqual(array, result)
     }
     
     func test_청소하기() {
         let num = 5
-        queue.enqueue(num)
-        queue.clear()
-        let result = queue.check()
+        intQueue.enqueue(num)
+        intQueue.clear()
+        let result = intQueue.check()
         XCTAssertEqual([], result)
     }
     
     func test_빈값확인하기() {
-        XCTAssertEqual(true, queue.isEmpty)
+        XCTAssertEqual(true, intQueue.isEmpty)
+    }
+    
+    func test_String추가하기() {
+        let word = "Hello"
+        stringQueue.enqueue(word)
+        guard let result = stringQueue.peek() else { return }
+        XCTAssertEqual(word, result)
+    }
+    
+    func test_String빼보기() {
+        let word = "World"
+        stringQueue.enqueue(word)
+        guard let result = stringQueue.dequeue() else {return}
+        XCTAssertEqual(result, word)
+    }
+    
+    func test_String큐청소하기() {
+        let word = "Swift"
+        stringQueue.enqueue(word)
+        stringQueue.clear()
+        let result = stringQueue.check()
+        XCTAssertEqual([], result)
+    }
+    
+    func test_Double추가하기() {
+        let double = 0.7
+        doubleQueue.enqueue(double)
+        guard let result = doubleQueue.peek() else { return }
+        XCTAssertEqual(double, result)
+    }
+    
+    func test_Double빼보기() {
+        let double = 0.5
+        doubleQueue.enqueue(double)
+        guard let result = doubleQueue.dequeue() else {return}
+        XCTAssertEqual(result, double)
+    }
+    
+    func test_Double큐청소하기() {
+        let double = 0.315454
+        doubleQueue.enqueue(double)
+        doubleQueue.clear()
+        let result = doubleQueue.check()
+        XCTAssertEqual([], result)
     }
 }
