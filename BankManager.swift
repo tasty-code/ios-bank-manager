82b0664dc6c650dcde133abef0ad5cea6622c958
JinUng41 <ingjwjw@naver.com>
2023-11-01 16:00:05 +0900

    feat: 메뉴 출력 및 고객 대기열 큐 구성하는 함수 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,30 @@
 //
 
 import Foundation
+
+final class BankManager {
+    private var clientQueue: Queue<Client> = Queue()
+    
+    func input() {
+        print(Prompt.menu, terminator: "")
+        guard let input = readLine() else { return }
+        switch input {
+        case "1":
+            break
+        case "2":
+            break
+        default:
+            print("입력이 잘못되었습니다.")
+            self.input()
+        }
+    }
+    
+    
+    private func makeClientQueue() {
+        let totalClient = Int.random(in: 10...30)
+
+        for num in 1...totalClient {
+            self.clientQueue.enqueue(data: Client(id: num))
+        }
+    }
+}
