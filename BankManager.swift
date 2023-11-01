81a19670c9f8309db6878e8aeed2ee3d556423db
JEON-Sungsu <anagma@naver.com>
2023-11-01 10:12:02 +0900

    feat: customer type, bankmanager type 생성

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,32 @@
 //
 
 import Foundation
+
+struct BankManager {
+    var userChoice: Int = 0
+    var isStart: Bool {
+        get {
+            if userChoice == 1 {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    
+    mutating func run() {
+        print("1 : 은행 개점\n2 : 종료")
+        print("입력 :", terminator: " ")
+        guard let input = readLine(), let number = Int(input) else {
+            return
+        }
+        
+        userChoice = number
+        
+        if isStart {
+            
+        } else {
+            
+        }
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,7 @@
 // 
 
 import Foundation
+
+let manager = BankManager()
+
+manager.run()
