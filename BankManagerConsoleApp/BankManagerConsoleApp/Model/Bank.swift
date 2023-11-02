a85bbaca4d529ac4b7c889f841f580ec3e081be4
JinUng41 <ingjwjw@naver.com>
2023-11-02 11:10:56 +0900

    feat: Bank 타입과 내부 중첩 타입으로 은행원을 의미하는 Teller 타입 구현
    Bank는 N명의 Teller를 가지기 때문에 배열로 저장하고 while문을 통해 대기열의 모든 고객을 처리할 때까지 동작하게 된다.

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -0,0 +1,52 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by 김진웅 on 2023/11/02.
+//
+
+import Foundation
+
+final class Bank {
+    private var tellers: [Teller] = []
+    private var count: Int = 0
+    private var totalTaskTime: Double = 0.0
+    private var clientQueue: Queue<Client> = Queue()
+    
+    init(numOfTellers: Int) {
+        tellers = (1...numOfTellers).map({ Teller(id: $0) })
+    }
+    
+    func makeClientQueue() {
+        let totalClient = Int.random(in: 10...30)
+        for num in 1...totalClient {
+            clientQueue.enqueue(data: Client(id: num))
+        }
+    }
+    
+    func open() {
+        while !clientQueue.isEmpty {
+            tellers.forEach { teller in
+                assignTask(to: teller)
+            }
+        }
+        print(Prompt.close(number: count, time: totalTaskTime))
+    }
+    
+    private func assignTask(to teller: Teller) {
+        guard let client = clientQueue.dequeue() else { return }
+        teller.performTask(with: client)
+        count += 1
+        totalTaskTime += Client.taskTime
+    }
+    
+    private struct Teller {
+        var id: Int
+        
+        func performTask(with client: Client) {
+            print(Prompt.taskStart(with: client))
+            Thread.sleep(forTimeInterval: Client.taskTime)
+            print(Prompt.taskComplete(with: client))
+        }
+    }
+}
