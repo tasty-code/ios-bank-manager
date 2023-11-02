263e195110fde8131d14e84dda1af20b87ab51c3
KimRoks <rudfhr3161@naver.com>
2023-11-02 20:26:52 +0900

    refactor: modify Unit Test
    - 가독성을 위한 given when then 패턴 적용
    - 명확한 의미전달을 위한 Test 네이밍 수정
    - 제네릭 테스트 케이스 분리

--- BankManagerConsoleApp/BankManagerConsoleAppTest/BankManagerConsoleAppTest.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTest/BankManagerConsoleAppTest.swift
@@ -22,82 +22,130 @@ final class BankManagerConsoleAppUnitTest: XCTestCase {
     }
     
     // MARK: - isEmpty Test
-    func test_아무것도_추가되지_않았을_경우() {
+    func test_아무것도_추가되지_않았을_경우_결과값_True() {
         XCTAssertTrue(sut.isEmpty)
     }
     
-    func test_데이터_한개_추가되었을_경우_False_반환() {
+    func test_데이터값_추가시_정상추가되면_isEmpty_False() {
+        // given
         let input = 123
+        
+        // when
         sut.enqueue(input)
         
+        // then
         XCTAssertFalse(sut.isEmpty)
     }
     
     // MARK: - enqueue Test
-    func test_데이터값_하나만_추가되었을_경우() {
+    func test_데이터값_하나만_추가되었을_경우_peek하면_input과_같음() {
+        // given
         let input = 123
+        
+        // when
         sut.enqueue(input)
         
+        // then
         XCTAssertEqual(input, sut.peek())
     }
     
-    func test_데이터값_두개_추가되었을_경우_연결성_확인() {
+    func test_데이터값_두개이상_추가되었을_경우_Peek와_input의_마지막값과_같음() {
+        // given
         let input = [123, 456, 789]
+        
+        // when
         input.forEach { sut.enqueue($0) }
+        
         let _ = sut.dequeue()
         
+        // then
         XCTAssertEqual(input[1], sut.peek())
     }
     
     // MARK: - dequeue Test
-    func test_데이터값_제거() {
+    func test_데이터값_두개이상_추가되었을_경우_dequeue가_FIFO로_작동함() {
+        // given
         let input = [123, 456, 789]
+        
+        // when
         input.forEach { sut.enqueue($0) }
         
+        // then
         XCTAssertEqual(input[0], sut.dequeue())
+        XCTAssertNotEqual(input[0], sut.peek())
     }
     
-    func test_데이터값_제거_후_리스트_비었는지_확인() {
+    func test_데이터값_제거_후_리스트가_isEmpty시_True() {
+        // given
         let input = 1
+        
+        // when
         sut.enqueue(input)
         let _ = sut.dequeue()
         
+        // then
         XCTAssertTrue(sut.isEmpty)
     }
     
     // MARK: - clear Test
-    func test_clear_확인() {
+    func test_clear시_리스트가_isEmpty시_True() {
+        // given
         let input = [123, 456, 789]
+        
+        // when
         input.forEach { sut.enqueue($0) }
         sut.clear()
         
+        // then
         XCTAssertTrue(sut.isEmpty)
     }
     
     // MARK: - Peek Test
-    func test_peek_확인() {
+    func test_peek시_input의_0번째_인덱스와_같음() {
+        // given
         let input = [123, 456, 789]
+        
+        // when
         input.forEach{ sut.enqueue($0) }
         
+        // then
         XCTAssertEqual(input[0], sut.peek())
     }
     
     // MARK: - Generic Test
-    func test_generic_타입_작동_확인() {
+    func test_generic에_String타입_적용시_Input과_peek이_같음() {
+        // given
         let stringQueue = Queue<String>()
-        let intQueue = Queue<Int>()
-        let doubleArrayQueue = Queue<Array<Double>>()
-        
         let stringInput: [String] = ["Test1", "테스트2", "✅3"]
+        
+        // when
+        stringInput.forEach { stringQueue.enqueue($0) }
+        
+        // then
+        XCTAssertEqual(stringInput[0], stringQueue.peek())
+    }
+    
+    func test_generic에_Int타입_적용시_Input과_peek이_같음() {
+        //given
+        let intQueue = Queue<Int>()
         let intInput: [Int] = [123, 456, 789]
+        
+        // when
+        intInput.forEach { intQueue.enqueue($0) }
+
+        //then
+        XCTAssertEqual(intInput[0], intQueue.peek())
+    }
+    
+    func test_generic에_Double타입_적용시_Input과_peek이_같음() {
+        //given
+        let doubleArrayQueue = Queue<Array<Double>>()
         let doubleArrayInput: [[Double]] = [[1.2, 2.3, 3.4], [3.55, 5.77, 7.99], [-0.33, -7.77, -10.11]]
         
-        stringInput.forEach { stringQueue.enqueue($0) }
-        intInput.forEach { intQueue.enqueue($0) }
+        //when
         doubleArrayInput.forEach { doubleArrayQueue.enqueue($0) }
         
-        XCTAssertEqual(stringInput[0], stringQueue.peek())
-        XCTAssertEqual(intInput[0], intQueue.peek())
+        //then
         XCTAssertEqual(doubleArrayInput[0], doubleArrayQueue.peek())
     }
 }
