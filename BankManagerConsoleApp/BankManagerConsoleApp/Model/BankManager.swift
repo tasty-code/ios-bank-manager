9841e9d771ac45c8bc9486bc8d40f5e7478a0df0
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-11-03 15:49:36 +0900

    [Feat] 업무 처리하는 startWork() 메서드 구현
    - semaphore를 활용해 은행원 수 설정

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManager.swift
@@ -9,8 +9,31 @@ import Foundation
 
 final class BankManager {
     private var clientQueue = Queue<Client>()
+    private var totalWorkTime = 0.0
+    private let semaphore: DispatchSemaphore
+    
+    init(_ clerkCount: Int) {
+        self.semaphore = DispatchSemaphore(value: clerkCount)
+    }
     
     func recept(client: Client) {
         self.clientQueue.enqueue(client)
+    }
+    
+    func startWork() {
+        while self.clientQueue.isEmpty == false {
+            self.semaphore.wait()
+            guard let client = self.clientQueue.dequeue() else { return }
+            DispatchQueue.global().async() {
+                self.task(for: client)
+                self.semaphore.signal()
+            }
+        }
+    }
+    
+    private func task(for client: Client) {
+        print(Script.WorkState.start(client: client))
+        self.totalWorkTime += client.task()
+        print(Script.WorkState.end(client: client))
     }
 }
