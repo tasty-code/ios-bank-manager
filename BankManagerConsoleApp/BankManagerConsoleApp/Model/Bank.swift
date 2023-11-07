50159f7befddd3afb84322348507e93e8bdb0744
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-11-07 11:44:55 +0900

    refactor: 세마포를 3개를 두어 작업을 비동기 처리하도록 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -9,15 +9,16 @@ import Foundation
 
 final class Bank {
     private var clientQueue: Queue<Client> = Queue()
-    private let tellers: [Teller] = [
-        Teller(taskType: .deposit),
-        Teller(taskType: .deposit),
-        Teller(taskType: .loan)
-    ]
     private let numberOfClient: Int = Int.random(in: 10...30)
-    private let semaphore: DispatchSemaphore = DispatchSemaphore(value: 1)
-    private let tellerGroup: DispatchGroup = DispatchGroup()
+    private let tellerSemaphore: DispatchSemaphore
+    private let depositSemaphore: DispatchSemaphore
+    private let loanSemaphore: DispatchSemaphore
     
+    init(depositTeller: Int, loanTeller: Int) {
+        tellerSemaphore = DispatchSemaphore(value: depositTeller + loanTeller)
+        depositSemaphore = DispatchSemaphore(value: depositTeller)
+        loanSemaphore = DispatchSemaphore(value: loanTeller)
+    }
     
     func visitClient() {
         for num in 1...numberOfClient {
@@ -26,46 +27,42 @@ final class Bank {
     }
     
     func open() {
-        tellers.forEach { teller in
-            DispatchQueue.global().async(group: tellerGroup) { [self] in
-                assignTask(to: teller)
+        let tellerGroup: DispatchGroup = DispatchGroup()
+        let dispatchQueue: DispatchQueue = DispatchQueue.global()
+        
+        while !clientQueue.isEmpty {
+            guard let client = clientQueue.dequeue() else {
+                return
+            }
+            tellerSemaphore.wait()
+            dispatchQueue.async(group: tellerGroup) { [weak self] in
+                self?.assignTask(with: client)
+                self?.tellerSemaphore.signal()
             }
         }
         tellerGroup.wait()
     }
     
-    func close(time: TimeInterval) {
+    func close(at time: Double) {
         print(Prompt.close(numberOfClient: numberOfClient, totalTaskTime: time))
     }
     
-    private func assignTask(to teller: Teller) {
-        while !clientQueue.isEmpty {
-            semaphore.wait()
-            
-            guard let clientTask = clientQueue.peek?.taskType,
-                  clientTask == teller.taskType
-            else {
-                semaphore.signal()
-                continue
-            }
-            
-            guard let client = clientQueue.dequeue() else {
-                return
-            }
-            
-            semaphore.signal()
-            
-            teller.performTask(with: client)
-        }
+    private func taskService(with client: Client) {
+        print(Prompt.taskStart(with: client))
+        Thread.sleep(forTimeInterval: client.taskType.taskTime)
+        print(Prompt.taskComplete(with: client))
     }
     
-    private struct Teller {
-        let taskType: TaskType
-        
-        func performTask(with client: Client) {
-            print(Prompt.taskStart(with: client))
-            Thread.sleep(forTimeInterval: client.taskType.taskTime)
-            print(Prompt.taskComplete(with: client))
+    private func assignTask(with client: Client) {
+        switch client.taskType {
+        case .loan:
+            loanSemaphore.wait()
+            taskService(with: client)
+            loanSemaphore.signal()
+        case .deposit:
+            depositSemaphore.wait()
+            taskService(with: client)
+            depositSemaphore.signal()
         }
     }
 }
