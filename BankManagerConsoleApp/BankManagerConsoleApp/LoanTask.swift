82d183e828102b6939b11865cbd838e389cf9e91
kimjunsung <assd1108@naver.com>
2023-11-07 11:35:55 +0900

    chore: var 프로퍼티 let 으로 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/LoanTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LoanTask.swift
@@ -10,6 +10,6 @@ import Foundation
 struct LoanTask: BankTask {
     static let semaphore: DispatchSemaphore = DispatchSemaphore(value: 1)
     static let dispatchQueue: DispatchQueue = DispatchQueue(label: "LoanQueue", attributes: .concurrent)
-    var name: String = "대출"
-    var processingTime: Double = 1.1
+    let name: String = "대출"
+    let processingTime: Double = 1.1
 }
