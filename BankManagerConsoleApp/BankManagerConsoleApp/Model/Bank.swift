e27313621e2fc97638c9393352754ee106e2b741
JinUng41 <ingjwjw@naver.com>
2023-11-07 13:44:13 +0900

    refactor: 고객에 대해 업무 타입으로 선분리, 후 업무처리
    각 업무에 따라 작업을 실행하는데 세마포 value에 따라 동시 실행할 수 있는 스레드의 수를 제한하였음.

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -9,60 +9,61 @@ import Foundation
 
 final class Bank {
     private var clientQueue: Queue<Client> = Queue()
-    private let numberOfClient: Int = Int.random(in: 10...30)
-    private let tellerSemaphore: DispatchSemaphore
     private let depositSemaphore: DispatchSemaphore
     private let loanSemaphore: DispatchSemaphore
+    private let tellerGroup: DispatchGroup = DispatchGroup()
+    private let dispatchQueue: DispatchQueue = DispatchQueue.global()
     
     init(depositTeller: Int, loanTeller: Int) {
-        tellerSemaphore = DispatchSemaphore(value: depositTeller + loanTeller)
         depositSemaphore = DispatchSemaphore(value: depositTeller)
         loanSemaphore = DispatchSemaphore(value: loanTeller)
     }
     
-    func visitClient() {
+    func visit(numberOfClient: Int) {
         for num in 1...numberOfClient {
             clientQueue.enqueue(data: Client(id: num))
         }
     }
     
     func open() {
-        let tellerGroup: DispatchGroup = DispatchGroup()
-        let dispatchQueue: DispatchQueue = DispatchQueue.global()
-        
         while !clientQueue.isEmpty {
             guard let client = clientQueue.dequeue() else {
                 return
             }
-            tellerSemaphore.wait()
-            dispatchQueue.async(group: tellerGroup) { [weak self] in
-                self?.assignTask(with: client)
-                self?.tellerSemaphore.signal()
+            
+            switch client.taskType {
+            case .deposit:
+                depositService(with: client)
+            case .loan:
+                loanService(with: client)
             }
         }
         tellerGroup.wait()
     }
     
-    func close(at time: Double) {
+    func close(numberOfClient: Int, at time: Double) {
         print(Prompt.close(numberOfClient: numberOfClient, totalTaskTime: time))
+    }
+    
+    private func depositService(with client: Client) {
+        dispatchQueue.async(group: tellerGroup) { [weak self] in
+            self?.depositSemaphore.wait()
+            self?.taskService(with: client)
+            self?.depositSemaphore.signal()
+        }
+    }
+    
+    private func loanService(with client: Client) {
+        dispatchQueue.async(group: tellerGroup) { [weak self] in
+            self?.loanSemaphore.wait()
+            self?.taskService(with: client)
+            self?.loanSemaphore.signal()
+        }
     }
     
     private func taskService(with client: Client) {
         print(Prompt.taskStart(with: client))
         Thread.sleep(forTimeInterval: client.taskType.taskTime)
         print(Prompt.taskComplete(with: client))
-    }
-    
-    private func assignTask(with client: Client) {
-        switch client.taskType {
-        case .loan:
-            loanSemaphore.wait()
-            taskService(with: client)
-            loanSemaphore.signal()
-        case .deposit:
-            depositSemaphore.wait()
-            taskService(with: client)
-            depositSemaphore.signal()
-        }
     }
 }
