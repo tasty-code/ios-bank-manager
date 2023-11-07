aacd75daeff1c13d6f66b1fd799e6e84351c684b
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2023-11-07 11:21:48 +0900

    feat : 기존의 main에 존재하던 isReady를 private로 만들기 위해 UserInterface 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/UserInterface.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct UserInterface {
+    
+   private var isReady = true
+
+   mutating func runProgram() {
+        while isReady {
+            print("1 : 은행 개점 \n2 : 종료")
+            print("입력 : ", terminator: "")
+            
+            guard let userInput = readLine() else { break }
+            
+            switch userInput {
+            case "1":
+                let bankManager = BankManager(depositTellerCount: 2, loanTellerCount: 1)
+                bankManager.openBank()
+                bankManager.finishTask()
+            case "2":
+                isReady = false
+            default:
+                print("잘못된 입력")
+            }
+        }
+    }
+}
