824c1c871467cfb66c6589a21f24f8fa8c6bc98d
COMDORI <02145s1@gmail.com>
2023-11-09 16:06:55 +0900

    refactor: 3차 리뷰 반영,  DispatchQueue 블럭 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Models/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Models/Bank.swift
@@ -10,12 +10,11 @@ import Foundation
 final class Bank {
     private let customerQueue = Queue<Customer>()
     private let dispatchGroup = DispatchGroup()
-    private let semaphore = DispatchSemaphore(value: 1)
-    private var tellers: [Teller]
+    private let tellers: [TellerProtocol]
     private var servicedCustomersCount = 0
     private var totalServicedTimes: TimeInterval = 0.0
     
-    init(tellers: [Teller]) {
+    init(tellers: [TellerProtocol]) {
         self.tellers = tellers
     }
     
@@ -23,23 +22,10 @@ final class Bank {
         gatherCustomers()
         
         tellers.forEach { teller in
-            DispatchQueue.global().async(group: dispatchGroup) { [self] in
-                while customerQueue.isEmpty == false {
-                    semaphore.wait()
-                    guard let customer = customerQueue.peek(), customer.workType == teller.workType else {
-                        semaphore.signal()
-                        continue
-                    }
-                    
-                    guard let customer = customerQueue.dequeue() else { return }
-                    
-                    semaphore.signal()
-                    
-                    teller.service(to: customer) { [self] in
-                        servicedCustomersCount += 1
-                        totalServicedTimes += customer.workType.timeCost
-                    }
-                }
+            while customerQueue.isEmpty == false {
+                guard let customer = customerQueue.dequeue() else { return }
+                
+                matchUpService(with: customer, on: teller)
             }
         }
         
@@ -54,8 +40,18 @@ extension Bank {
     private func gatherCustomers() {
         let totalCustomersCount = Int.random(in: 10...30)
         for count in 1...totalCustomersCount {
-            let customer = Customer(id: count)
+            guard let randomWorkType = WorkType.allCases.randomElement() else { return }
+            let customer = Customer(id: count, workType: randomWorkType)
             customerQueue.enqueue(customer)
+        }
+    }
+    
+    private func matchUpService(with customer: Customer, on teller: TellerProtocol) {
+        DispatchQueue.global().async(group: dispatchGroup) {
+            teller.service(to: customer) { [self] in
+                servicedCustomersCount += 1
+                totalServicedTimes += customer.workType.timeCost
+            }
         }
     }
 }
