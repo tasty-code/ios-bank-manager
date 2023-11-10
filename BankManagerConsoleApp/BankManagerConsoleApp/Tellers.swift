8ebf5facbcd5404554d13e8a6174e53f88997797
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2023-11-10 15:29:54 +0900

    refactor : review 반영

--- BankManagerConsoleApp/BankManagerConsoleApp/Tellers.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Tellers.swift
@@ -8,33 +8,26 @@ struct Tellers {
         self.tellerCount = tellerCount
         self.tellerType = tellerType
     }
-
+    
     func doTask(queue: Queue<Int>) {
         let name = tellerType.name
         let time = tellerType.time
         
         let semaphore = DispatchSemaphore(value: tellerCount)
         let group = DispatchGroup()
-        var workingTellerCount = 0
         
         while !queue.isEmpty() {
+            
+            guard let data = queue.dequeue() else { return }
+            
             semaphore.wait()
-            guard let data = queue.dequeue() else {
+            group.enter()
+            DispatchQueue.global().async {
+                print("\(name) 은행원 \(data)번 고객 \(name)업무 시작")
+                usleep(time)
+                print("\(name) 은행원 \(data)번 고객 \(name)업무 완료")
                 semaphore.signal()
-                return
-            }
-
-            if workingTellerCount < tellerCount {
-                group.enter()
-                DispatchQueue.global().async {
-                    workingTellerCount += 1
-                    print("\(name) 은행원 \(data)번 고객 \(name)업무 시작")
-                    usleep(time)
-                    print("\(name) 은행원 \(data)번 고객 \(name)업무 완료")
-                    workingTellerCount -= 1
-                    semaphore.signal()
-                    group.leave()
-                }
+                group.leave()
             }
         }
         group.wait()
