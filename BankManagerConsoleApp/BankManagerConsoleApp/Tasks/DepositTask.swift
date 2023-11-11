8a16e00e3415c43e935f33f4340cf11ea29d7926
JJong <jfknfj920@naver.com>
2023-11-11 17:42:32 +0900

    refactor: Taskable을 채택하는 모든 struct에 private init 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Tasks/DepositTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Tasks/DepositTask.swift
@@ -4,4 +4,6 @@ struct DepositTask: Taskable {
     static let name: String = "입금"
     static let semaphore: DispatchSemaphore = DispatchSemaphore(value: 2)
     static let processingTime: Double = 0.7
+    
+    private init() {}
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/Tasks/LoanTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Tasks/LoanTask.swift
@@ -4,4 +4,6 @@ struct LoanTask: Taskable {
     static let name: String = "대출"
     static let semaphore: DispatchSemaphore = DispatchSemaphore(value: 1)
     static let processingTime: Double = 1.1
+    
+    private init() {}
 }
