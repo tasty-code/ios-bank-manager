1c636c59c1c395cf55a2aab8228ece3d760d2eac
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-11-13 10:57:36 +0900

    config: 코드 UI를 위한 편의 생성자 구현

---
+++ BankManagerUIApp/BankManagerUIApp/UIComponentsExtension.swift
@@ -0,0 +1,47 @@
+//
+//  UIComponentsExtension.swift
+//  BankManagerUIApp
+//
+//  Created by 김수경 on 2023/11/13.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont, textColor: UIColor = .label, textAlignment: NSTextAlignment = .natural) {
+        self.init()
+        
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UIButton {
+    convenience init(title: String, titleColor: UIColor, backgroundColor: UIColor = .clear) {
+        self.init()
+        
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UIStackView {
+    convenience init(axis: NSLayoutConstraint.Axis, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fillEqually, spacing: CGFloat = 0.0, subViews: [UIView] = []) {
+        self.init()
+        
+        self.axis = axis
+        self.alignment = alignment
+        self.distribution = distribution
+        self.spacing = spacing
+        subViews.forEach {
+            self.addArrangedSubview($0)
+        }
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
