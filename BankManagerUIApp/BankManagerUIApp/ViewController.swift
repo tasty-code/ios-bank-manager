27fa369eb958ce9abfff81b614684612b0da8654
SimJaeHyeok <jaehyeoking@gmail.com>
2023-11-13 21:45:04 +0900

    feat: 타이머, 초기화, 고객 10명 추가 버튼 구현

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -1,25 +1,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var waitingQueueStackView: UIStackView!
+    var workingQueueStackView: UIStackView!
+    var timer: Timer?
+    var elapsedTime: TimeInterval = 0.000
+    
+    
+    let workTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "업무시간 - 00:00:000"
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
         
         let tenCustomerAddButton = UIButton(title: "고객 10명 추가", titleColor: .systemBlue)
         let initializationButton = UIButton(title: "초기화", titleColor: .systemRed)
         let customerInitializationStackView = UIStackView(axis: .horizontal, distribution: .fillEqually, arrangedSubviews: [tenCustomerAddButton, initializationButton])
         
-        let workTimeLabel = UILabel(text: "업무시간 - 00:00:000", fontSize: 20, textColor: .black)
-                
+        workTimeLabel.textAlignment = .center
+        
         let waitingLabel = UILabel(text: "대기중", fontSize: 30, textColor: .white, backgroundColor: .systemGreen)
         let workingLabel = UILabel(text: "업무중", fontSize: 30, textColor: .white, backgroundColor: .systemIndigo)
         let waitingWorkingLabelStackView = UIStackView(axis: .horizontal, distribution: .fillEqually, spacing: 0, arrangedSubviews: [waitingLabel, workingLabel])
         
-        let waitingQueueStackView = UIStackView(axis: .vertical, distribution: .fillEqually, arrangedSubviews: addCustomer(until: 100))
+        waitingQueueStackView = UIStackView(axis: .vertical, distribution: .fillEqually, arrangedSubviews: addWaitCustomer(3))
         let waitingQueue = UIScrollView(subView: waitingQueueStackView)
- 
-        let workingQueueStackView =  UIStackView(axis: .vertical, distribution: .fill, alignment: .fill, arrangedSubviews: addCustomer(until: 3))
+        
+        workingQueueStackView =  UIStackView(axis: .vertical, distribution: .fill, alignment: .fill, arrangedSubviews: addWaitCustomer(3))
         let workingQueue = UIScrollView(subView: workingQueueStackView, isScrollEnabled: false)
 
         let waitingWorkingQueueStackView = UIStackView(axis: .horizontal, distribution: .fillEqually, spacing: 0, arrangedSubviews: [waitingQueue, workingQueue])
@@ -48,18 +63,56 @@ class ViewController: UIViewController {
             workingQueueStackView.topAnchor.constraint(equalTo: workingQueue.topAnchor),
             workingQueueStackView.bottomAnchor.constraint(equalTo: workingQueue.bottomAnchor),
             workingQueueStackView.widthAnchor.constraint(equalTo: workingQueue.widthAnchor),
-            
         ])
+        
+        tenCustomerAddButton.addTarget(self, action: #selector(addCustomer), for: .touchUpInside)
+        initializationButton.addTarget(self, action: #selector(initializeCustomer), for: .touchUpInside)
     }
-}
-
-func addCustomer(until maxCustomer: Int) -> [UIView] {
-    var views: [UIView] = []
     
-    for index in 1...maxCustomer {
-        views.append(UILabel(text: "\(index)번", textColor: .black))
+    @objc func addCustomer() {
+        for index in 1...10 {
+            let label = UILabel(text: "\(index)번 고객", fontSize: 10, textColor: .black)
+            waitingQueueStackView.addArrangedSubview(label)
+        }
+        if let timer = timer {
+            
+        } else {
+            timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        }
     }
-    return views
+    
+    @objc func initializeCustomer() {
+        waitingQueueStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        workingQueueStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        elapsedTime = 0.000
+        timer?.invalidate()
+        timer = nil
+        workTimeLabel.text = "업무시간 - 00:00:000"
+        }
+    
+    @objc func updateTimer() {
+        
+        let minutes = Int(elapsedTime) / 60
+        let seconds = Int(elapsedTime) % 60
+        let milliseconds = Int((elapsedTime.truncatingRemainder(dividingBy: 1)) * 1000)
+        
+        workTimeLabel.font = UIFont(name: "HelveticaNeue", size: 20)
+        workTimeLabel.text = String(format: "업무시간 - %02d:%02d:%03d", minutes, seconds, milliseconds)
+        elapsedTime += 0.001
+    }
+    
+    func addWaitCustomer(_ maxCustomer: Int) -> [UIView] {
+        var views = [UIView]()
+        for index in 1...maxCustomer {
+            let customer = UILabel()
+            customer.text = "\(index)번 고객"
+            customer.textAlignment = .center
+            customer.font = .systemFont(ofSize: 10)
+            views.append(customer)
+        }
+        return views
+    }
 }
 
 extension UIButton {
@@ -90,6 +143,11 @@ extension UILabel {
 }
 
 extension UIStackView {
+    func addArrangedSubviews(_ addArrangedSubviews: [UIView]) {
+        arrangedSubviews.forEach { $0.removeFromSuperview() }
+        addArrangedSubviews.forEach { addArrangedSubview($0) }
+    }
+    
     convenience init(axis: NSLayoutConstraint.Axis, distribution: UIStackView.Distribution, alignment: UIStackView.Alignment = .fill, spacing: CGFloat = 8, arrangedSubviews: [UIView] = []) {
         self.init()
         
@@ -101,7 +159,7 @@ extension UIStackView {
         arrangedSubviews.forEach {
             self.addArrangedSubview($0)
         }
-
+        
         self.isLayoutMarginsRelativeArrangement = false
         self.isUserInteractionEnabled = true
         self.translatesAutoresizingMaskIntoConstraints = false
