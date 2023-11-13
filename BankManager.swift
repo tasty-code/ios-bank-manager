b06d49650d338ad40a50a8986c6f8cdf20d7da7a
kimjunsung <assd1108@naver.com>
2023-11-13 09:41:13 +0900

    fix: BankManager 복구

---
+++ BankManager.swift
@@ -0,0 +1,43 @@
+//
+//  BankManager.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+final class BankManager {
+    private let bank: Bankable
+    private var userChoice: Int = 0
+    
+    var isStart: Bool {
+        if userChoice == 1 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init(bank: Bankable) {
+        self.bank = bank
+    }
+    
+    func startBusiness() {
+        while true {
+            print(BankDialogue.menu)
+            print(BankDialogue.input, terminator: "")
+            guard let input = readLine(), let number = Int(input) else {
+                return
+            }
+            
+            userChoice = number
+            
+            if isStart {
+                let (taskProcessingTime, handledCustomer) = bank.beginTask()
+                print(BankDialogue.close(handledCustomer, workTime: taskProcessingTime))
+            } else {
+                break
+            }
+        }
+    }
+}
