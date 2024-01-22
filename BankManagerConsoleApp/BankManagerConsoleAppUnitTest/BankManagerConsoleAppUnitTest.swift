ac23a439e04e051413c3e702153d69f6952c71ff
suojae3 <suojae3@gmail.com>
2024-01-22 16:00:15 +0900

    feat: Linked List 테스트 코드 파일 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleAppUnitTest/BankManagerConsoleAppUnitTest.swift
@@ -0,0 +1,15 @@
+
+import XCTest
+@testable import BankManagerConsoleApp
+
+final class LinkedListUnitTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+}
