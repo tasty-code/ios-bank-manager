3dd9b4a6b6b142378220485d45d532ab6e2c72fd
suojae3 <suojae3@gmail.com>
2024-01-23 10:14:42 +0900

    chore: 사용하지 않는 LinkedList메서드 삭제

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -31,13 +31,7 @@ final class LinkedList<T> {
 // MARK: - Methods
 extension LinkedList {
     
-    /// Add
-    func addFront(_ data: T) {
-        let newNode = Node(data)
-        newNode.next = head
-        head = newNode
-    }
-    
+    /// Add    
     func addBack(_ data: T) {
         let newNode = Node(data)
         
@@ -54,35 +48,7 @@ extension LinkedList {
         node?.next = newNode
     }
     
-    func insert(position: Int, data: T) {
-        if position == 0 {
-            addFront(data)
-            return
-        }
-        
-        let newNode = Node(data)
-        var currentNode = head
-        
-        for _ in 0..<position-1 {
-            currentNode = currentNode?.next
-        }
-        newNode.next = currentNode?.next
-        currentNode?.next = newNode
-    }
-    
     /// Get
-    func getLast() -> T? {
-        if head == nil {
-            return nil
-        }
-        var node = head
-        
-        while(node?.next != nil) {
-            node = node?.next
-        }
-        return node?.data
-    }
-    
     func getFirst() -> T? {
         return head?.data
     }
@@ -91,34 +57,7 @@ extension LinkedList {
     func removeFirst() {
         head = head?.next
     }
-    
-    func removeLast() {
-        var nextNode = head
-        var previousNode: Node<T>?
-
-        while(nextNode?.next != nil) {
-            previousNode = nextNode
-            nextNode = nextNode?.next
-        }
-        previousNode?.next = nil
-    }
-    
-    func remove(at position: Int) {
-        if position == 0 {
-            self.removeFirst()
-            return
-        }
-        
-        var nextNode = head
-        var previousNode: Node<T>?
-        
-        for _ in 0..<position {
-            previousNode = nextNode
-            nextNode = nextNode?.next
-        }
-        previousNode?.next = nextNode?.next
-    }
-    
+            
     func removeAll() {
         head = nil
     }
