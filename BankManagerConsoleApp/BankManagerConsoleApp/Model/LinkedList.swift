aa95deecb691e52c81b2d6171013e597890a2f8e
angryeon7 <nayeon7612@gmail.com>
2024-01-23 15:22:31 +0900

    feat: LinkedList 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/LinkedList.swift
@@ -0,0 +1,49 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by nayeon  on 2024/01/23.
+//
+
+import Foundation
+
+final class LinkedList<Element> {
+    private(set) var head: Node<Element>?
+    private(set) var tail: Node<Element>?
+    private(set) var count: Int = 0
+    
+    var isEmpty: Bool {
+        head == nil
+    }
+    
+    func append(item: Element) {
+        let newNode = Node(data: item)
+        
+        guard let node = tail else {
+            head = newNode
+            tail = newNode
+            count += 1
+            return
+        }
+        node.next = newNode
+        tail = newNode
+        count += 1
+    }
+    
+    func remove() -> Element? {
+        let removeNodeValue = head?.data
+        
+        head = head?.next
+        tail = isEmpty ? head : tail
+        count = isEmpty ? count : count - 1
+        
+        return removeNodeValue
+    }
+    
+    func removeAll() {
+        head = nil
+        tail = nil
+        count = 0
+    }
+}
+
