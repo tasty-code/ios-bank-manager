5b60ff4e539f48400c07df903c54f88d649c118c
Effie <effie.thedev@gmail.com>
2024-01-23 17:21:43 +0900

    test: LinkedList 테스트 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleAppTest/LinkedListTest.swift
@@ -0,0 +1,20 @@
+//
+//  LinkedListTest.swift
+//  BankManagerConsoleAppTest
+//
+//  Created by Effie on 1/23/24.
+//
+
+import XCTest
+@testable import BankManagerConsoleApp
+
+final class LinkedListTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
