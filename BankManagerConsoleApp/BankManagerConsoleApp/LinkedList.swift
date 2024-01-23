79ec011ed6b3ccd7eae493509a93038cd815c85f
eensungkim <eensungkim@gmail.com>
2024-01-23 17:29:40 +0900

    feat: LinkedList 클래스 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -34,3 +34,82 @@ extension Node {
         return lhs.getValue() == rhs.getValue()
     }
 }
+
+class LinkedList<T: Equatable> {
+    private var head: Node<T>?
+    private var _count: Int
+    var count: Int {
+        return _count
+    }
+    
+    init(head: Node<T>? = nil) {
+        self.head = head
+        self._count = head == nil ? 0 : 1
+    }
+}
+
+extension LinkedList {
+    public func getNode(index: Int) -> Node<T>? {
+        if index < 0 || index > _count {
+            fatalError("Index out of range")
+        }
+        
+        if index == 0 {
+            return head
+        }
+        
+        var nextNode: Node<T>? = head
+
+        for _ in 1...index {
+            guard let next = nextNode?.getNext() else {
+                return nil
+            }
+            nextNode = next
+        }
+        return nextNode
+    }
+    
+    public func getReferenceNode(_ node: Node<T>) -> Node<T>? {
+        if let reference = head?.getNext(), reference == node {
+            return head
+        }
+        
+        var nextNode = head?.getNext()
+            
+        for _ in 1..._count {
+            if let reference = nextNode?.getNext(), reference == node {
+                return nextNode
+            }
+            nextNode = nextNode?.getNext()
+        }
+        
+        return nil
+    }
+    
+    public func addNode(_ node: Node<T>) {
+        let lastNode = getNode(index: _count - 1)
+        lastNode?.changeNext(to: node)
+        _count += 1
+    }
+    
+    public func addNode(_ node: Node<T>, after: Node<T>) {
+        if let temporaryNode = after.getNext() {
+            node.changeNext(to: temporaryNode)
+        }
+        
+        after.changeNext(to: node)
+        _count += 1
+    }
+    
+    public func removeNode(_ node: Node<T>) {
+        let previousNode = getReferenceNode(node)
+        
+        if let nextNode = node.getNext() {
+            previousNode?.changeNext(to: nextNode)
+        } else {
+            previousNode?.changeNext(to: nil)
+        }
+      
+        _count -= 1
+    }
+}
