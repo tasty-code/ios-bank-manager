edd3c33b3f2a649162a91b512aae9df43e51f73c
eensungkim <eensungkim@gmail.com>
2024-01-23 19:41:46 +0900

    chore: Node 와 LinkedList 파일 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -1,34 +1,6 @@
 
 import Foundation
 
-class Node<T: Equatable> {
-    private let _value: T
-    var value: T {
-        return _value
-    }
-    
-    private var _next: Node?
-    var next: Node? {
-        get {
-            return _next
-        }
-        set(node) {
-           _next = node
-        }
-    }
-    
-    init(value: T, next: Node? = nil) {
-        self._value = value
-        self._next = next
-    }
-}
-
-extension Node {
-    static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
-        return lhs.value == rhs.value
-    }
-}
-
 class LinkedList<T: Equatable> {
     private var head: Node<T>?
     private var _count: Int
--- BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class Node<T: Equatable> {
+    private let _value: T
+    var value: T {
+        return _value
+    }
+    
+    private var _next: Node?
+    var next: Node? {
+        get {
+            return _next
+        }
+        set(node) {
+           _next = node
+        }
+    }
+    
+    init(value: T, next: Node? = nil) {
+        self._value = value
+        self._next = next
+    }
+    
+    static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
+        return lhs.value == rhs.value
+    }
+}
