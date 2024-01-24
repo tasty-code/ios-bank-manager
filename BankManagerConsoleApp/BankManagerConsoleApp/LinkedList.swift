d35885309070f27356a7be9ed218fe1b55635fbc
jus1234 <osjamg1996@gmail.com>
2024-01-24 10:26:10 +0900

    chore: 이전 커밋 도중 delete된 파일 복구

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,77 @@
+class Node<T> {
+    var data: T
+    var next: Node?
+    
+    init(data: T, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+class LinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    func append(data: T) {
+        let newNode = Node(data: data)
+        if let tailNode = tail {
+            tailNode.next = newNode
+        } else {
+            head = newNode
+        }
+        tail = newNode
+    }
+    
+    func insert(data: T, at index: Int) {
+        let newNode = Node(data: data)
+        if index == 0 {
+            newNode.next = head
+            head = newNode
+            if tail == nil {
+                tail = newNode
+            }
+        } else {
+            var current = head
+            for _ in 0..<(index - 1) {
+                current = current?.next
+            }
+            if current == nil {
+                append(data: data)
+                return
+            }
+            newNode.next = current?.next
+            current?.next = newNode
+        }
+    }
+    
+    private func removeFirst() -> T? {
+        if let headNode = head {
+            head = headNode.next
+            if head == nil {
+                tail = nil
+            }
+            return headNode.data
+        } else {
+            return nil
+        }
+    }
+    
+    func remove(at index: Int) -> T? {
+        if head == nil { return nil }
+        if index == 0 {
+            return removeFirst()
+        }
+        var node = head
+        for _ in 0..<(index - 1) {
+            if node?.next?.next == nil { break }
+            node = node?.next
+        }
+        let removedNode = node?.next
+        node?.next = node?.next?.next
+        return removedNode?.data
+    }
+}
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedListQueue.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class LinkedListQueue<Element>: Queue {
+    private var queue: LinkedList<Element> = LinkedList()
+    private var count: Int = 0
+    var isEmpty: Bool = true
+    
+    func enqueue(element: Element) {
+        
+    }
+    
+    func dequeue() -> Element? {
+        
+    }
+    
+    func clear() {
+        
+    }
+    
+    func peek() -> Element? {
+        
+    }
+    
+    
+}
