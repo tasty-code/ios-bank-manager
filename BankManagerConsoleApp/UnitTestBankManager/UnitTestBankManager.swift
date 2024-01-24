5bec34e0d7bf8e9edf27521997610b951f9a93c0
rlwhs12 <150293158+rlwhs12@users.noreply.github.com>
2024-01-24 16:04:13 +0900

    feat:UnitTest 생성

---
+++ BankManagerConsoleApp/UnitTestBankManager/UnitTestBankManager.swift
@@ -0,0 +1,21 @@
+//
+//  UnitTestBankManager.swift
+//  UnitTestBankManager
+//
+//  Created by MAC2020 on 2024/01/24.
+//
+
+import XCTest
+@testable import BankManagerConsoleApp
+
+final class UnitTestBankManager: XCTestCase {
+    var sut: Queue<Int>!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
