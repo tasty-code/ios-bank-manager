4bf1dc4f14c9a03a781343db4bf25a52816d47f7
hemil0102 <hemil0102@gmail.com>
2024-01-24 20:54:54 +0900

    test: main console창에서 Queue 기능 테스트 코드 작성 (#1)

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,4 +6,32 @@
 
 import Foundation
 
+let queue: Queue<String> = Queue()
+
+print("1️⃣ queue.isEmpty: \(queue.isEmpty())")
+
+queue.enqueue(with: "A")
+queue.enqueue(with: "B")
+queue.enqueue(with: "C")
+print("2️⃣ queue.enqueue")
+dump(queue)
+print("3️⃣ queue.totalLength: \(queue.totalLength())")
+
+if let dequeuedItem = queue.dequeue() {
+    print("4️⃣ queue.dequeue: \(dequeuedItem)")
+    dump(queue)
+    print("5️⃣ queue.totalLength: \(queue.totalLength())")
+}
+
+if let peekedItem = queue.peek() {
+    print("6️⃣ queue.peek: \(peekedItem)")
+    dump(queue)
+}
+
+print("7️⃣ queue.isEmpty: \(queue.isEmpty())")
+
+queue.clean()
+print("8️⃣ queue.isEmpty: \(queue.isEmpty())")
+
+
 
