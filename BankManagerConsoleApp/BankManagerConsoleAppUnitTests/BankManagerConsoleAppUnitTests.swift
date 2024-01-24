794422a26518afab9d850bc9138dadf94898a355
jus1234 <osjamg1996@gmail.com>
2024-01-24 16:29:54 +0900

    chore: 이전 커밋에서 발생한 delete 오류 수정(삭제된 파일 추가)

---
+++ BankManagerConsoleApp/BankManagerConsoleAppUnitTests/BankManagerConsoleAppUnitTests.swift
@@ -0,0 +1,50 @@
+//
+//  BankManagerConsoleAppUnitTests.swift
+//  BankManagerConsoleAppUnitTests
+//
+//  Created by 장우석 on 1/24/24.
+//
+
+import XCTest
+@testable import BankManagerConsoleApp
+
+final class BankManagerConsoleAppUnitTests: XCTestCase {
+
+    var sut: LinkedListQueue<String>!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = LinkedListQueue()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_큐에_세개의_요소를_차례로_인큐하면_차례대로_디큐된다() {
+        // given
+        let firstElement: String = "1"
+        let secondElement: String = "2"
+        let thirdlement: String = "3"
+        let expectation: Array<String> = ["1", "2", "3"]
+        // when
+        sut.enqueue(element: firstElement)
+        sut.enqueue(element: secondElement)
+        sut.enqueue(element: thirdlement)
+        var enqueueResult: Array<String> = []
+        enqueueResult.append(sut.dequeue()!)
+        enqueueResult.append(sut.dequeue()!)
+        enqueueResult.append(sut.dequeue()!)
+        // then
+        XCTAssertEqual(expectation, enqueueResult)
+    }
+    
+    func test_큐에_아무것도_인큐하지_않으면_큐는_비어있다() {
+        // given
+        let expectation: Bool = true
+        // when
+        let result: Bool = sut.isEmpty
+        // then
+        XCTAssertEqual(expectation, result)
+    }
+}
