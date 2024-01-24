485ca4c92ff77df9e5b5c604de7749ea64d7fd69
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-24 20:28:54 +0900

    feat: LinkedList 클래스 pop, clean, peek 구현 (#1)

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -34,4 +34,26 @@ class LinkedList<T> {
         count += 1
         tail = tail?.next
     }
+    
+    func pop() -> T? {
+        let returnValue = head?.data
+        head = head?.next
+        if isEmpty {
+            tail = nil
+            count = 0
+            return nil
+        }
+        count -= 1
+        return returnValue
+    }
+    
+    func clean() {
+        head = nil
+        tail = nil
+        count = 0
+    }
+    
+    func peek() -> T? {
+        head?.data
+    }
 }
