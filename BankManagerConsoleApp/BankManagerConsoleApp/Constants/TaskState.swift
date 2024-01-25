b257b182bdbe22b06d9e02b6b2f1fc1178957908
eunjuchoi <eunjuzzoo@naver.com>
2024-01-25 17:15:23 +0900

    feat: TaskState 열거형 정의

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Constants/TaskState.swift
@@ -0,0 +1,13 @@
+//
+//  TaskState.swift
+//  BankManagerConsoleApp
+//
+//  Created by EUNJU on 2024/01/25.
+//
+
+import Foundation
+
+enum TaskState {
+    case start(number: Int)
+    case finish(number: Int)
+}
