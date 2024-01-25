942b0e37e556fc5693ee88f760aa25e4f463b800
wlsdud-dev <hjymail1025@gmail.com>
2024-01-25 11:18:55 +0900

    feat: 링크드리스트 지정 노드 삭제, 마지막 노드 삭제, 노드 카운트 계산 기능 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -13,6 +13,18 @@ struct LinkedList<T> {
         return head == nil
     }
     
+    mutating func count() -> UInt {
+        var count: UInt = 1
+        var node = head
+        
+        while node?.next != nil {
+            node = node?.next
+            count += 1
+        }
+        
+        return count
+    }
+    
     mutating func append(value :T) {
         let newNode = Node(value: value)
         
@@ -47,7 +59,7 @@ struct LinkedList<T> {
             var node = head
             for _ in 1...at {
                 if node?.next == nil {
-                    tail = node?.next
+                    tail = newNode
                     break
                 }
                 node = node?.next
@@ -56,6 +68,42 @@ struct LinkedList<T> {
             node?.next = newNode
             node?.next?.next = nextNode
         }
+    }
+    
+    mutating func remove(at index: UInt) -> Node<T>? {
+        if isEmpty() { return nil }
+        
+        var node = head
+        if index == 0 {
+            head = node?.next
+            head?.next = node?.next?.next
+            return node
+        }
+        
+        for _ in 0..<index-1 {
+            if node?.next?.next == nil {
+                tail = node?.next
+                break
+            }
+            node = node?.next
+        }
+        let removedNode = node?.next
+        node?.next = node?.next?.next
+        return removedNode
+    }
+    
+    mutating func removeLast() -> Node<T>? {
+        if isEmpty() { return nil }
+        
+        var node = head
+        
+        while node?.next?.next != nil {
+            node = node?.next
+        }
+        let removedNode = node?.next
+        node?.next = node?.next?.next
+        tail = node
+        return removedNode
     }
     
     mutating func removeAll() {
