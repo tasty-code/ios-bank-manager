b7dec27c590c437b49a65d65029837cdaf0c4fd0
ujhong7 <ujhong7>
2024-01-25 19:58:30 +0900

    feat: Queue 타입을 Linked-list으로 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,50 @@
 //
 
 import Foundation
+
+class Node<T> {
+    var value: T
+    var next: Node?
+
+    init(value: T) {
+        self.value = value
+    }
+}
+
+struct Queue<T> {
+    private var front: Node<T>?
+    private var rear: Node<T>?
+
+    mutating func enqueue(_ value: T) {
+        let newNode = Node(value: value)
+        if isEmpty() {
+            front = newNode
+            rear = newNode
+        } else {
+            rear?.next = newNode
+            rear = newNode
+        }
+    }
+
+    mutating func dequeue() -> T? {
+        guard let currentFront = front else { return nil }
+        front = currentFront.next
+        if front == nil {
+            rear = nil
+        }
+        return currentFront.value
+    }
+
+    func peek() -> T? {
+        return front?.value
+    }
+
+    func isEmpty() -> Bool {
+        return front == nil
+    }
+
+    mutating func clear() {
+        front = nil
+        rear = nil
+    }
+}
