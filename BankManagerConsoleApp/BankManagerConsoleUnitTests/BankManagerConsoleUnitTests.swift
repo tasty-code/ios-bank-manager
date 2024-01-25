853eb92764932fb8c5c52aa02a508781c4dfe6a3
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-01-25 10:50:13 +0900

    feat: clear test코드 구현

--- BankManagerConsoleApp/BankManagerConsoleUnitTests/BankManagerConsoleUnitTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleUnitTests/BankManagerConsoleUnitTests.swift
@@ -13,17 +13,24 @@ final class BankManagerConsoleUnitTests: XCTestCase {
         sut = nil
     }
     
-    func test_값을_처음_추가하면_헤드가_존재해야_한다() {
+    /// append
+    func test_첫_노드를_추가하면_리스트가_비어있지_않아야_한다() {
         // given
-        let element = "3"
-        
-        // when
-        sut.appendNode(value: element)
+        sut.appendNode(value:  "3")
         
         // then
         XCTAssertEqual(sut.isEmpty, false)
     }
+    /// append
+    func test_첫_노드를_추가하면_헤드의_값과_같아야_한다() {
+        // given
+        sut.appendNode(value: "1")
+        
+        // then
+        XCTAssertEqual(sut.headValue(), "1")
+    }
     
+    /// removeFirst
     func test_노드가_두개이상일때_removeFirst를하면_두번째노드가_헤드가_된다() {
         // given
         sut.appendNode(value: "1")
@@ -35,4 +42,34 @@ final class BankManagerConsoleUnitTests: XCTestCase {
         // then
         XCTAssertEqual(sut.head?.value, "폴")
     }
+    
+    /// clear
+    func test_노드를추가하고_클리어를_했을때_head와_tail이_nil이어야_한다() {
+        // given
+        sut.appendNode(value: "1")
+        sut.appendNode(value: "2")
+        sut.appendNode(value: "3")
+        
+        // when
+        sut.clear()
+        
+        // then
+        XCTAssertEqual(sut.head == nil, true)
+        XCTAssertEqual(sut.tail == nil, true)
+    }
+    
+    /// clear
+    func test_노드를추가하고_클리어를_했을때_노드의_다음값이_없어야_한다() {
+        // given
+        sut.appendNode(value: "1")
+        sut.appendNode(value: "2")
+        sut.appendNode(value: "3")
+        
+        // when
+        sut.clear()
+        
+        // then
+        XCTAssertEqual(sut.head?.next == nil, true)
+    }
+
 }
