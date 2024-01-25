045e841274ae6165f744495099ea1f3160f82bdd
MyNB1 <dishdib61@naver.com>
2024-01-25 20:47:44 +0900

    feat: 유닛테스트 메서드 추가

--- BankManagerConsoleApp/QueueTest/QueueTest.swift
+++ BankManagerConsoleApp/QueueTest/QueueTest.swift
@@ -8,29 +8,77 @@
 import XCTest
 @testable import BankManagerConsoleApp
 
-final class QueueTest: XCTestCase {
+final class QueueTests: XCTestCase {
+    typealias List = LinkedList<Int>
+    
+    var sut: Queue<Int>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = Queue(queue: List())
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_queue에값이없을떄_peek호출시_nil을반환한다() {
+        //given
+
+        //when
+        let result = sut.peek()
+        //then
+        XCTAssertNil(result)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_queue에값이1과2가있을때_clear호출시_빈배열true를반환한다() {
+        //given
+        sut.enqueue(element: 1)
+        sut.enqueue(element: 2)
+        //when
+        sut.clear()
+        //then
+        XCTAssertTrue(sut.isEmpty())
+    }
+
+    func test_queue에1과2가있을때_첫번째로1을반환하고마지막으로2를반환한다() {
+        //given
+        let first = 1
+        let second = 2
+        //when
+        sut.enqueue(element: first)
+        sut.enqueue(element: second)
+        //then
+        XCTAssertEqual(try sut.dequeue(), first)
+        XCTAssertEqual(try sut.dequeue(), second)
+    }
+
+    func test_queue에1과2가있을때_dequeue호출했을때_첫번쨰값인2가반환된다() {
+        //given
+        let firstElement = 2
+        let secondElement = 1
+        let secondNode = Node(data: secondElement)
+        let firstNode = Node(data: firstElement, next: secondNode)
+        let list = List(head: firstNode, tail: secondNode)
+        sut = Queue(queue: list)
+        
+        do {
+            //when
+            let result = try sut.dequeue()
+            //then
+            XCTAssertEqual(result, firstElement)
+        } catch {
+           print("error")
         }
     }
-
+    
+    func test_queue에1과2와3이있을때_remove호출하여_2를반환한다() {
+        //given
+        sut.enqueue(element: 1)
+        sut.enqueue(element: 2)
+        sut.enqueue(element: 3)
+        //when
+        let removeElement = sut.queue.remove(index: 1)
+        //then
+        XCTAssertEqual(removeElement, 2)
+    }
 }
