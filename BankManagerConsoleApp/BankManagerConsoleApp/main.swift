d2eb5b4d6a5eaf60c889f88739a068c8514d5bce
Effie <effie.thedev@gmail.com>
2024-01-26 11:34:00 +0900

    refine: 메서드 묶기, 디버깅용 속성 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -27,29 +27,39 @@ final class CustomerDispatcher: CustomerDispatchable {
 }
 
 final class Banker {
+    private let name: String
+    
     private let dispatcher: CustomerDispatchable
     
-    init(dispatcher: CustomerDispatchable) {
+    private let queue = DispatchQueue.global()
+    
+    init(name: String, dispatcher: CustomerDispatchable) {
+        self.name = name
         self.dispatcher = dispatcher
     }
     
     func start(group: DispatchGroup) {
-        DispatchQueue.global().async(group: group) { [weak self] in
+        // 얘를 아이템으로 만들면 좋을 듯
+        queue.async(group: group) { [weak self] in
             guard let self else { return }
             while let newComer = dispatcher.dispatchCustomer() {
-                self.newComerStart(number: newComer.number)
-                sleep(1)
-                self.newComerEnd(number: newComer.number)
+                work(for: newComer, time: 0.7)
             }
         }
     }
     
+    private func work(for newComer: Customer, time: Double) {
+        self.newComerStart(number: newComer.number)
+        Thread.sleep(forTimeInterval: time)
+        self.newComerEnd(number: newComer.number)
+    }
+    
     private func newComerStart(number: Int) {
-        print("\(number)번 고객 업무 시작")
+        print("\(self.name): \(number)번 고객 업무 시작")
     }
     
     private func newComerEnd(number: Int) {
-        print("\(number)번 고객 업무 완료")
+        print("\(self.name): \(number)번 고객 업무 완료")
     }
 }
 
@@ -66,13 +76,13 @@ func test() {
     let list = makeList()
     let customerDispatcher: CustomerDispatchable = CustomerDispatcher(customerQueue: Queue(linkedList: list))
     
-    let bankers = [
-        Banker(dispatcher: customerDispatcher),
-        Banker(dispatcher: customerDispatcher),
-        Banker(dispatcher: customerDispatcher),
-    ]
-    
     let group = DispatchGroup()
+    
+    var bankers: [Banker] = [
+        Banker.init(name: "에피", dispatcher: customerDispatcher),
+        Banker.init(name: "카일", dispatcher: customerDispatcher),
+        Banker.init(name: "디오", dispatcher: customerDispatcher),
+    ]
     
     for banker in bankers {
         banker.start(group: group)
@@ -80,7 +90,5 @@ func test() {
     
     group.wait()
 }
-
-
 
 test()
