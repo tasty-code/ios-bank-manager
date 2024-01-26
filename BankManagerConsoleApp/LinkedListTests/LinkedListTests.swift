3a37ce8d0eb96ee981b5fd7c4efc38879fef320a
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-01-26 10:24:56 +0900

    test: BankTellerQueueTest 실패 케이스 추가

--- BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
+++ BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
@@ -23,16 +23,16 @@ final class LinkedListTests: XCTestCase {
     
     func test_빈List에append호출시_2를추가하면_head값에2가할당되는지() {
         //when
-        sut.append(item: 2)
+        sut.append(new: 2)
         //then
         XCTAssertEqual(sut.head?.data, 2)
     }
     
     func test_5가있는List에append호출시_7을추가하면_nexthead에7이할당되는지() {
         //given
-        sut.append(item: 5)
+        sut.append(new: 5)
         //when
-        sut.append(item: 7)
+        sut.append(new: 7)
         //then
         XCTAssertEqual(sut.head?.next?.data, 7)
     }
@@ -51,8 +51,8 @@ final class LinkedListTests: XCTestCase {
     
     func test_List에_3와9추가후_removeAll호출시_isEmpty가true되는지() {
         //given
-        sut.append(item: 3)
-        sut.append(item: 9)
+        sut.append(new: 3)
+        sut.append(new: 9)
         //when
         sut.removeAll()
         //then
@@ -64,7 +64,7 @@ final class LinkedListTests: XCTestCase {
     
     func test_1가있는List에_count요청시_1반환하는지() {
         //given
-        sut.append(item: 1)
+        sut.append(new: 1)
         //when
         let result = sut.count
         //then
@@ -73,7 +73,7 @@ final class LinkedListTests: XCTestCase {
     
     func test_List에4추가후_tail과head요청시_4반환하는지() {
         //given
-        sut.append(item: 4)
+        sut.append(new: 4)
         //when
         let tail = sut.tail?.data
         let head = sut.head?.data
--- BankManagerConsoleApp/QueueTest/BankTellerQueueTest.swift
+++ BankManagerConsoleApp/QueueTest/BankTellerQueueTest.swift
@@ -21,7 +21,7 @@ final class BankTellerQueueTest: XCTestCase {
         bankTellerQueue = nil
     }
     
-    func test_queue에enqueue호출후_1을_넣고_peek호출시_1을_반환하는지() {
+    func test_queue에_enqueue호출후_1을_넣고_peek호출시_1을_반환하는지() {
         //given
         let result = 1
         bankTellerQueue.enqueue(item: result)
@@ -31,7 +31,7 @@ final class BankTellerQueueTest: XCTestCase {
         XCTAssertEqual(peekResult, result)
     }
     
-    func test_queue에enqueue호출후_1를_추가하면_dequeue호출시_1을_반환하는지() {
+    func test_queue에_enqueue호출후_1를_추가하면_dequeue호출시_1을_반환하는지() {
         //given
         let result = 1
         bankTellerQueue.enqueue(item: result)
@@ -42,7 +42,7 @@ final class BankTellerQueueTest: XCTestCase {
         XCTAssertEqual(bankTellerQueue.dequeue(), nil)
     }
     
-    func test_queue가비어있고_dequeue호출시_nil을반환하는지() {
+    func test_queue가_비어있고_dequeue호출시_nil을반환하는지() {
         //given
         //when
         guard bankTellerQueue.isEmpty() else {
@@ -54,7 +54,7 @@ final class BankTellerQueueTest: XCTestCase {
         XCTAssertEqual(dequeueResult, nil)
     }
     
-    func test_queue에enqueue호출후_123을_넣고_clear후_peek을_호출시_nil을_반환하는지() {
+    func test_queue에_enqueue호출후_123을_넣고_clear후_peek을_호출시_nil을_반환하는지() {
         //given
         for value in 1...3 {
             bankTellerQueue.enqueue(item: value)
@@ -66,7 +66,7 @@ final class BankTellerQueueTest: XCTestCase {
         XCTAssertEqual(peekResult, nil)
     }
     
-    func test_queue에enqueue호출후_4를_넣고_clear호출시_isEmpty가true_count가_0을_반환하는지() {
+    func test_queue에_enqueue호출후_4를_넣고_clear호출시_isEmpty가true_count가_0을_반환하는지() {
         //given
         bankTellerQueue.enqueue(item: 4)
         //when
@@ -78,19 +78,37 @@ final class BankTellerQueueTest: XCTestCase {
         XCTAssertEqual(countResult, 0)
     }
     
+    func test_queue에_enqueue호출후_4를_넣고_isEmpty가false_count가_1을_반환하는지() {
+        //given
+        bankTellerQueue.enqueue(item: 4)
+        //when
+        let isEmptyResult = bankTellerQueue.isEmpty()
+        let countResult = bankTellerQueue.count
+        //then
+        XCTAssertTrue(isEmptyResult)
+        XCTAssertEqual(countResult, 0)
+    }
+    
     func test_queue에_isEmpty호출시_true를_반환하는지() {
         //given
         let result = true
-        let anotherResult = false
         //when
         let isEmptyResult = bankTellerQueue.isEmpty()
-        bankTellerQueue.enqueue(item: 1)
         //then
         XCTAssertEqual(isEmptyResult, result)
-        XCTAssertEqual(bankTellerQueue.isEmpty(), anotherResult)
     }
     
-    func test_queue에enqueue호출후_1부터15를넣고_10이_될때까지_dequeue시_10을_반환하는지() {
+    func test_queue에_enqueue호출후_1을_넣고_isEmpty호출시_false를_반환하는지() {
+        //given
+        let result = true
+        //when
+        bankTellerQueue.enqueue(item: 1)
+        let isEmptyResult = bankTellerQueue.isEmpty()
+        //then
+        XCTAssertEqual(isEmptyResult, result)
+    }
+    
+    func test_queue에_enqueue호출후_1부터15를넣고_10이_될때까지_dequeue시_10을_반환하는지() {
         //given
         var findNumber = 0
         let result = 10
@@ -107,7 +125,7 @@ final class BankTellerQueueTest: XCTestCase {
         XCTAssertEqual(dequeueResult, result)
     }
     
-    func test_queue애enqueue_1을넣고_2번_dequeue시_nil을_반환하는지() {
+    func test_queue애_enqueue_1을넣고_2번_dequeue시_nil을_반환하는지() {
         //given
         var dequeueResult: Int?
         bankTellerQueue.enqueue(item: 1)
