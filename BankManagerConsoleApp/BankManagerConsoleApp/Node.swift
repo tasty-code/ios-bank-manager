1b78e8093214303551ac407a58dee42e156a0b56
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-28 14:05:55 +0900

    refactor: Node 클래스 final 키워드 및 은닉화 적용 (#1)

--- BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -1,10 +1,26 @@
 
-class Node<T> {
-    var data: T?
-    var next: Node<T>?
+final class Node<T> {
+    private var data: T?
+    private var next: Node<T>?
     
     init(data: T? = nil, next: Node<T>? = nil) {
         self.data = data
         self.next = next
+    }
+    
+    func updateData(data: T?) {
+         self.data = data
+    }
+    
+    func updateNext(next: Node<T>?) {
+         self.next = next
+    }
+    
+    func readData() -> T? {
+        return self.data
+    }
+    
+    func readNext() -> Node<T>? {
+        return self.next
     }
 }
