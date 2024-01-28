bb4de76ecb2bfaae2ce9f96c2ddf5a972a1ce8fd
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-28 15:22:58 +0900

    refactor: Unit Test Given/When/Then 적용 2차 적용 (#1)

--- BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
@@ -103,69 +103,77 @@ final class BankManagerConsoleAppTests: XCTestCase {
         XCTAssertNil(result)
     }
     
-    ///⬇️ L 리팩토링 고고
     func test_dequeue한_데이터가_삭제된다() {
-        let firstInput = "First"
-        let secondInput = "Second"
-        
-        sut.enqueue(with: firstInput)
-        sut.enqueue(with: secondInput)
-        
-        sut.dequeue()
-        let result = sut.peek()
-        
-        XCTAssertEqual(secondInput, result)
+        // given
+        let input = [ "First", "Second" ]
+        for data in input {
+            sut.enqueue(with: data)
+        }
+        let _ = sut.dequeue()
+        // when
+        let result = sut.peek() == "Second"
+        // then
+        XCTAssertTrue(result)
     }
     
     func test_dequeue시_삭제되는_첫데이터_확인() {
-        let firstInput = "First"
-        let secondInput = "Second"
-        
-        sut.enqueue(with: firstInput)
-        sut.enqueue(with: secondInput)
-        
-        let result = sut.dequeue()
-        
-        XCTAssertEqual(firstInput, result)
+        // given
+        let input = [ "First", "Second" ]
+        for data in input {
+            sut.enqueue(with: data)
+        }
+        //when
+        let result = sut.dequeue() == "First"
+        // then
+        XCTAssertTrue(result)
     }
     
     func test_첫노드를_peek한다() {
-        let firstInput = "First"
-        let secondInput = "Second"
-        
-        sut.enqueue(with: firstInput)
-        sut.enqueue(with: secondInput)
-        
-        let result = sut.peek()
-        
-        XCTAssertEqual(firstInput, result)
-    }
-    
-    func test_큐를_clear한다() {
-        let _ = sut.enqueue(with: "A")
-        let _ = sut.enqueue(with: "B")
-        let _ = sut.enqueue(with: "C")
-        let _ = sut.enqueue(with: "D")
-        let _ = sut.clean()
-        let result = sut.isEmpty
+        // given
+        let input = [ "First", "Second" ]
+        for data in input {
+            sut.enqueue(with: data)
+        }
+        // when
+        let result = sut.peek() == "First"
         
         XCTAssertTrue(result)
     }
-
+    
+    func test_큐를_clear한다() {
+        // given
+        let input = [ "A", "B", "C", "D" ]
+        for data in input {
+            sut.enqueue(with: data)
+        }
+        let _ = sut.clean()
+        // when
+        let result = sut.isEmpty
+        // then
+        XCTAssertTrue(result)
+    }
+    
     func test_노드가_정상적으로_다음_노드를_연결한다() {
-        sut.enqueue(with: "A")
-        sut.enqueue(with: "B")
-        sut.enqueue(with: "C")
-        
+        // given
+        let input = [ "A", "B", "C" ]
+        for data in input {
+            sut.enqueue(with: data)
+        }
+        // when
         let result = sut.linkedList.head?.next?.next?.data
+        // then
         XCTAssertEqual("C", result)
     }
     
     func test_tail의_다음_연결이_nil이다() {
-        sut.enqueue(with: "A")
-        sut.enqueue(with: "B")
-        
+        // given
+        let input = [ "A", "B", "C" ]
+        for data in input {
+            sut.enqueue(with: data)
+        }
+        // when
         let result = sut.linkedList.tail?.next
+        // then
         XCTAssertNil(result)
     }
 }
