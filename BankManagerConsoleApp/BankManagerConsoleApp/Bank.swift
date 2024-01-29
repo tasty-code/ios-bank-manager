cdeed1b4666c5d72c936999c1d344778d17c6eeb
jus1234 <osjamg1996@gmail.com>
2024-01-29 11:06:42 +0900

    feat: Bank 클래스 프로퍼티 및 생성자 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -1,3 +1,10 @@
 final class Bank {
+    private var customerCount: Int
+    private var customerQueue: LinkedListQueue<Customer>
+    private let customerCountRange: ClosedRange<Int> = 10...30
     
+    init() {
+        self.customerCount = 0
+        self.customerQueue = LinkedListQueue<Customer>()
+    }
 }
