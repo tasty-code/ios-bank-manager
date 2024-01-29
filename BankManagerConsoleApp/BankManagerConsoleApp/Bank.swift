4b4bbfc237cf2201e3cd0f37196df7e988f1bac9
jus1234 <osjamg1996@gmail.com>
2024-01-29 11:11:36 +0900

    feat: 사용자 입력 처리 기능 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,3 +8,30 @@ final class Bank {
         self.customerQueue = LinkedListQueue<Customer>()
     }
 }
+
+extension Bank {
+    func process() {
+        Message.menu.printMessage()
+        Message.input.printMessage()
+        
+        guard let selectedMenu = selectMenu() else {
+            Message.wrongInput.printMessage()
+            process()
+            return
+        }
+        
+        if selectedMenu == Menu.exit.value {
+            return
+        }
+        
+    }
+    
+    private func selectMenu() -> Int? {
+        guard let input = readLine(),
+              let selectedMenu = Int(input),
+              (Menu.open.value...Menu.exit.value) ~= selectedMenu else {
+            return nil
+        }
+        return selectedMenu
+    }
+}
