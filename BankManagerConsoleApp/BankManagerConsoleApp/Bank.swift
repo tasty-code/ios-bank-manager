58ebf8d03164e48cb27e1ca3c1fdb564c804cf8a
MyNB1 <dishdib61@naver.com>
2024-01-29 16:32:26 +0900

    feat: openBank 업무시간 계산 기능 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -18,15 +18,18 @@ struct Bank {
     
     func openBank() {
         setCustomerCount(customer: customNum)
+        let startWorkTime: CFAbsoluteTime = CFAbsoluteTimeGetCurrent()
         bankManager.assign()
-        closeBank()
+        let endOfWorkTime: CFAbsoluteTime = CFAbsoluteTimeGetCurrent()
+        let workTime = String(format: "%.2f", endOfWorkTime - startWorkTime)
+        closeBank(time: workTime)
     }
     
     func setCustomerCount(customer: Int) {
         (1...customer).forEach { bankManager.standBy(customer: Customer(numOfPerson: $0)) }
     }
     
-    func closeBank() {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customNum)명이며, 총 업무시간은 초 입니다.")
+    func closeBank(time: String) {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customNum)명이며, 총 업무시간은 \(time)초 입니다.")
     }
 }
