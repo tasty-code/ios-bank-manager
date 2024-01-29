cea8086c50761bb2170ac75804199c6e93225198
mireu <148876644+mireu930@users.noreply.github.com>
2024-01-29 21:12:20 +0900

    refactor: private을 통한 접근제어

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct Bank {
-    let bankManager = BankManager()
-    let bankClerk = BankClerk()
+    private let bankManager = BankManager()
+    private let bankClerk = BankClerk()
     let customNum: Int
     
     init(customNum: Int) {
@@ -22,18 +22,18 @@ struct Bank {
         closeBank(time: workTime)
     }
     
-    func setCustomerCount(customer: Int) {
+    private func setCustomerCount(customer: Int) {
         (1...customer).forEach { bankManager.standBy(customer: Customer(numOfPerson: $0)) }
     }
     
-    func calulateWorkTime(work: () -> Void) -> String {
+    private func calulateWorkTime(work: () -> Void) -> String {
         let startTime = CFAbsoluteTimeGetCurrent()
         work()
         let endTime = CFAbsoluteTimeGetCurrent()
         return String(format: "%.2f", endTime - startTime)
     }
     
-    func closeBank(time: String) {
+    private func closeBank(time: String) {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customNum)명이며, 총 업무시간은 \(time)초 입니다.")
     }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankClerk.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct BankClerk {
-    var pace: Double = 0.7
+    private let pace: Double = 0.7
     
     func recieve(customer: Customer) {
         print("\(customer.numOfPerson)번 고객 업무 시작")
