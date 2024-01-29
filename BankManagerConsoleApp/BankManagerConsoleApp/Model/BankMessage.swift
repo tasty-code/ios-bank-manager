75332fa1cdd3ea369520f712e55f16dcd307bdfd
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-01-29 16:30:54 +0900

    feat: BankMessage 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankMessage.swift
@@ -0,0 +1,28 @@
+enum BankMessage {
+    case menu
+    case wrongInput
+    case taskStart(with: Int)
+    case taskComplete(with: Int)
+    case close(customersCount: Int, totalProcessingTime: Double)
+}
+
+extension BankMessage {
+    func message() {
+        switch self {
+        case .menu:
+            print("""
+            1 : 은행개점
+            2 : 종료
+            입력 :
+            """, terminator: " ")
+        case .wrongInput:
+            print("1 혹은 2 를 입력하세요.")
+        case .taskStart(let customer):
+            print("\(customer)번 고객 업무 시작")
+        case .taskComplete(let customer):
+            print("\(customer)번 고객 업무 종료")
+        case .close(let customersCount, let totalProcessingTime):
+            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customersCount)명이며, 총 업무시간은 \(totalProcessingTime)초입니다.")
+        }
+    }
+}
