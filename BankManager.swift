17cc6f601182ac632b949cdf13abb453cc1a6e57
NERO <mxzxro@gmail.com>
2024-01-29 15:43:18 +0900

    feat: 메뉴 출력 메서드 생성
    readLine 추가

--- BankManager.swift
+++ BankManager.swift
@@ -12,4 +12,18 @@ struct BankManager {
     
     private let banker: Banker = Banker()
     private let queue: Queue<Customer> = Queue(linkedList: LinkedList())
+    
+    init(totalCustomers: Int, totalTime: Double) {
+        self.totalCustomers = totalCustomers
+        self.totalTime = totalTime
+    }
+    
+    func printMenu() {
+        print("""
+            1 : 은행 개점
+            2 : 종료
+            입력 :
+            """, terminator: " ")
+        let selectedMenu = readLine()
+    }
 }
