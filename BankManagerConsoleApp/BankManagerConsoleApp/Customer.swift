55252197cc523292bfdcabb2f8125cd306555ba1
wlsdud-dev <hjymail1025@gmail.com>
2024-01-30 14:56:34 +0900

    feat: 고객 객체 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,14 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jin-Mac on 1/30/24.
+//
+
+struct Customer {
+    let number: Int
+    
+    init(number: Int) {
+        self.number = number
+    }
+}
