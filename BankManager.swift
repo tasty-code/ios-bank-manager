9916f4c2c90c986278018e1fb8adbd81602a08ea
hemil0102 <hemil0102@gmail.com>
2024-01-30 15:50:51 +0900

    feat: 콘솔창 은행 메뉴 출력 구현 (#2)

--- BankManager.swift
+++ BankManager.swift
@@ -4,3 +4,30 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+struct BankManager {
+    var isOpen: Bool = true
+    
+    func printMenuOfBank() {
+        print("1 : 은행개점")
+        print("2 : 종료")
+    }
+    
+    mutating func operateBank() {
+        while isOpen {
+            printMenuOfBank()
+            isOpen = false
+        }
+    }
+    
+    mutating func executeBankingOperation(of input: String) {
+        switch input {
+        case "1": 
+            print("은행개점")
+        case "2":
+            print("은행종료")
+            isOpen = false
+        default:
+            break
+        }
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -4,29 +4,7 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-var queue: Queue<String> = Queue()
+var bank: BankManager = BankManager()
+bank.operateBank()
 
-print("1️⃣ queue.isEmpty: \(queue.isEmpty)")
 
-queue.enqueue(with: "A")
-queue.enqueue(with: "B")
-queue.enqueue(with: "C")
-print("2️⃣ queue.enqueue")
-dump(queue)
-print("3️⃣ queue.totalLength: \(queue.totalLength())")
-
-if let dequeuedItem = queue.dequeue() {
-    print("4️⃣ queue.dequeue: \(dequeuedItem)")
-    dump(queue)
-    print("5️⃣ queue.totalLength: \(queue.totalLength())")
-}
-
-if let peekedItem = queue.peek() {
-    print("6️⃣ queue.peek: \(peekedItem)")
-    dump(queue)
-}
-
-print("7️⃣ queue.isEmpty: \(queue.isEmpty)")
-
-queue.clean()
-print("8️⃣ queue.isEmpty: \(queue.isEmpty)")
