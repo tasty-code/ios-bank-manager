b7817e03333acdb7b04ae1df0f616b2e5a544dd5
eunjuchoi <eunjuzzoo@naver.com>
2024-01-30 11:18:08 +0900

    chore: 유저 입력 값이 특정 범위 내에 존재하는지 확인하는 코드 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -12,6 +12,7 @@ final class Bank {
     private let bankManager: BankManager
     private var customerCount: Int?
     private let startNumber: Int = 1
+    private let userChoiceRange: ClosedRange<Int> = 1...2
     private let customerCountRange: ClosedRange<Int> = 10...30
     
     init(bankManager: BankManager) {
@@ -36,7 +37,7 @@ final class Bank {
     private func validateUserInput(with userInput: String?) throws -> Int? {
         guard let userInput,
               let userChoice = Int(userInput),
-              userChoice > 0 && userChoice < 3
+              userChoiceRange.contains(userChoice)
         else {
             throw BankError.inputError
         }
