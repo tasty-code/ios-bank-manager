7b39fef054b13f2d932fb3192a779917344bd5ea
Chan Ho Park <park_ch@kakao.com>
2024-01-30 15:26:20 +0900

    feat: Customer 타입 구현.

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -0,0 +1,15 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by 박찬호 on 1/30/24.
+//
+
+/// 고객의 고유 번호 설정
+struct Customer {
+    let waitingNumber: Int
+    
+    init(waitingNumber: Int) {
+        self.waitingNumber = waitingNumber
+    }
+}
