47c0a1e08c8739fe06b98dcf2faca65d41a4e9a9
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-01-30 14:52:48 +0900

    refactor: BankMessage clase케이스 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankMessage.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankMessage.swift
@@ -3,7 +3,7 @@ enum BankMessage {
     case wrongInput
     case taskStart(with: Customer)
     case taskComplete(with: Customer)
-    case close(customersCount: Int, totalProcessingTime: String)
+    case close(customersCount: Int, totalProcessingTime: Double)
 }
 
 extension BankMessage {
@@ -18,7 +18,7 @@ extension BankMessage {
         case .taskComplete(let customer):
             print("\(customer.waitingNumber)번 고객 업무 종료")
         case .close(let customersCount, let totalProcessingTime):
-            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customersCount)명이며, 총 업무시간은 \(totalProcessingTime)초입니다.")
+            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customersCount)명이며, 총 업무시간은 \(String(format: "%.2f", totalProcessingTime))초입니다.")
         }
     }
 }
