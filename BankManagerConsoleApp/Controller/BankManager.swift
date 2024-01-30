d30ef8f4838c7736b87bff969ee1dbe4336174eb
LeeSe0ngYe0n <goblin__@naver.com>
2024-01-30 17:13:44 +0900

    feat: 모든 고객 업무 처리 이후 소요된 업무시간 출력 기능 구현 (#2)

--- BankManagerConsoleApp/Controller/BankManager.swift
+++ BankManagerConsoleApp/Controller/BankManager.swift
@@ -9,11 +9,16 @@ struct BankManager {
     var ticketMachine = Queue<Customer>()
     var customers: [Customer] = []
     var employees: [Employee] = []
+    var totalTaskTime: Double { Double(customers.count * 700) / 1000 }
     
     func printMenuOfBank() {
         print("1 : 은행개점")
         print("2 : 종료")
         print("입력 : ", terminator: "")
+    }
+    
+    func printCloseMenu() {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customers.count)명이며, 총 업무시간은 \(totalTaskTime)초 입니다.")
     }
     
     mutating func operateBank() {
@@ -35,6 +40,7 @@ struct BankManager {
             print("\(ticketMachine.totalLength())명의 고객이 대기열이 등록되었다")
             employees.append(Employee())
             employees[0].handleCustomerTasks(with: ticketMachine)
+            printCloseMenu()
         case "2":
             print("은행종료")
             isOpen = false
