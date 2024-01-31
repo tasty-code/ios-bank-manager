0666b40be28b9a8fc17c4cd250b21065537327d7
yoruck2 <yoruck2@gmail.com>
2024-02-01 02:33:19 +0900

    feat: "입력 : " 출력 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -12,6 +12,7 @@ func operateBank() {
     while true {
         print("1 : 은행 개점")
         print("2 : 종료")
+        print("입력 : ",terminator: "")
 
         if let choice = readLine(), let menu = Int(choice) {
             switch menu {
