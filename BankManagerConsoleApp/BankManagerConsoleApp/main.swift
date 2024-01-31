ec07b48912dc0c754ed33ec7c86abf960a029a95
yoruck2 <yoruck2@gmail.com>
2024-01-31 17:28:05 +0900

    feat: operation 기능 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,29 @@
 // 
 
 import Foundation
+
+func operateBank() {
+    let bank = Bank()
+
+    while true {
+        print("1 : 은행 개점")
+        print("2 : 종료")
+
+        if let choice = readLine(), let menu = Int(choice) {
+            switch menu {
+            case 1:
+                bank.open()
+            case 2:
+                print("프로그램을 종료합니다.")
+                return
+            default:
+                print("올바른 메뉴를 선택해주세요.")
+            }
+        } else {
+            print("숫자를 입력해주세요.")
+        }
+    }
+}
+
+
+operateBank()
