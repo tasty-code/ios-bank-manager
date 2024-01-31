0f5b78c054cc8a89ff8a3850910e27b6aecd430d
ujhong7 <ujhong7>
2024-01-31 16:19:35 +0900

    feat: Queue 타입 구현을 위한 Linked-list 타입 구현 후 분리

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/LinkedList.swift
@@ -0,0 +1,53 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by yujaehong on 1/31/24.
+//
+
+import Foundation
+
+class LinkedList<T> {
+    var front: Node<T>?
+    var rear: Node<T>?
+    
+    init(front: Node<T>? = nil, rear: Node<T>? = nil) {
+        self.front = front
+        self.rear = rear
+    }
+    
+    var first: T? {
+        return front?.value
+    }
+    
+    var isEmpty: Bool {
+        return front == nil
+    }
+}
+
+extension LinkedList {
+    func append(_ value: T) {
+        let newNode = Node(value)
+        
+        if isEmpty {
+            front = newNode
+            rear = front
+        } else {
+            rear?.next = newNode
+            rear = newNode
+        }
+    }
+    
+    func removeFirst() -> T? {
+        guard let currentFront = front else { return nil }
+        front = front?.next
+        return currentFront.value
+    }
+    
+    func removeAll() {
+        front = nil
+        rear = nil
+    }
+}
+
+
