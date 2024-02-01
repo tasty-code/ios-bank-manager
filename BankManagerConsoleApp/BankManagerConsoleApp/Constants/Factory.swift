4ba3a58e33a5ea2252aca0eb3ef25cd8d04bc963
suojae3 <suojae3@gmail.com>
2024-02-01 16:14:49 +0900

    feat: MemberFactory 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Constants/Factory.swift
@@ -0,0 +1,21 @@
+//
+//  Factory.swift
+//  BankManagerConsoleApp
+//
+//  Created by ㅣ on 2/1/24.
+//
+
+import Foundation
+
+struct MemberFactory {
+    
+    func makeCustomers(count: Int) -> [Customer] {
+        var customers = [Customer]()
+        for number in 1...count {
+            let service: BankService = Bool.random() ? .deposit : .loan
+            let customer = Customer(number: number, service: service)
+            customers.append(customer)
+        }
+        return customers
+    }
+}
