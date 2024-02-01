74fdd608377ee0e804a98ed87becdfe4e16b7fec
eensungkim <eensungkim@gmail.com>
2024-02-01 13:30:29 +0900

    refactor: completionHandler에 업무 완료 print 전달하도록 변경

--- BankManager.swift
+++ BankManager.swift
@@ -29,7 +29,9 @@ struct BankManager {
             
             switch selectedMenu {
             case "1":
-                startBankingProcess()
+                startBankingProcess { customer, time in
+                    print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customer)명이며, 총 업무시간은 \(String(format: "%.2f", time))초입니다.")
+                }
             case "2":
                 isExit = true
             default:
@@ -39,7 +41,7 @@ struct BankManager {
         }
     }
     
-    mutating private func startBankingProcess() {
+    mutating private func startBankingProcess(completionHandler: (_ customer: Int, _ time: Double) -> Void) {
         setupInitialInformation()
         
         while !queue.isEmpty() {
@@ -52,7 +54,7 @@ struct BankManager {
             }
         }
         
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomers)명이며, 총 업무시간은 \(String(format: "%.2f", totalTime))초입니다.")
+        completionHandler(totalCustomers, totalTime)
     }
     
     mutating private func setupInitialInformation() {
