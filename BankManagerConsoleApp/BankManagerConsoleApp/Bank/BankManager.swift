6ba1a404e4c2eef9e45ba8f91c8f805c4b67d538
Paul Lee <earths-voluble.0g@icloud.com>
2024-02-01 11:45:32 +0900

    refactor: readLine 메서드 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
@@ -8,7 +8,7 @@ final class BankManager {
     func openBank() {
         while true {
             print("1 : 은행개점\n2 : 종료\n입력 :", terminator: " ")
-            guard let choice = readLine() else { return }
+            let choice = getUserInput()
             switch choice {
             case "1":
                 banker.taskStart()
@@ -18,5 +18,9 @@ final class BankManager {
                 print("1 혹은 2를 입력하세요.")
             }
         }
+    }
+    
+    private func getUserInput() -> String? {
+        return readLine()
     }
 }
