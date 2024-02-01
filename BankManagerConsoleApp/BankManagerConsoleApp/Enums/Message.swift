f60c2e79710bd0076b41a3cbbf94843856b61357
jus1234 <osjamg1996@gmail.com>
2024-02-01 14:18:26 +0900

    feat: Message enum 수정(task 별 메시지 추가)

--- BankManagerConsoleApp/BankManagerConsoleApp/Enums/Message.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Enums/Message.swift
@@ -2,8 +2,8 @@ enum Message {
     case menu
     case input
     case wrongInput
-    case start(number: Int)
-    case complete(number: Int)
+    case start(number: Int, task: Task)
+    case complete(number: Int, task: Task)
     case close(customerCount: Int, time: Double)
 }
 
@@ -19,10 +19,10 @@ extension Message {
             print("입력 : ", terminator: "")
         case .wrongInput:
             print("잘못된 입력입니다. 다시 입력해주세요.")
-        case .start(let number):
-            print("\(number)번 고객 업무 시작")
-        case .complete(let number):
-            print("\(number)번 고객 업무 완료")
+        case .start(let number, let task):
+            print("\(number)번 고객 \(task.korean)업무 시작")
+        case .complete(let number, let task):
+            print("\(number)번 고객 \(task.korean)업무 완료")
         case .close(let customerCount, let time):
             print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(String(format: "%.2f", time))초입니다.")
         }
