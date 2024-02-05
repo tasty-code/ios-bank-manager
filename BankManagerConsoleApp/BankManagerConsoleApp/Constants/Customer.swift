2fdd857b3490a8382862d3da61a2e2526518e067
suojae3 <suojae3@gmail.com>
2024-02-05 16:50:05 +0900

    chore: Customer 객체에 업무시간 포함

--- BankManagerConsoleApp/BankManagerConsoleApp/Constants/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Constants/Customer.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
-enum BankService {
-    case deposit
-    case loan
+enum BankService: String {
+
+    case loan = "대출"
+    case deposit = "예금"
+    
+    var requiredTime: Double {
+        switch self {
+        case .loan:
+            return 1.1
+        case .deposit:
+            return 0.7
+        }
+    }
 }
 
 struct Customer {
