09e4440967e2c54376e422dfb546e232c017e87e
yoruck2 <yoruck2@gmail.com>
2024-02-05 11:21:43 +0900

    refactor: print문 enum을 통한 메세지 출력으로 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -4,28 +4,25 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+
 
 func operateBank() {
-    let bank = Bank()
+    let bank = Bank(depositBankerCount: 1, loanBankerCount: 2)
 
     while true {
-        print("1 : 은행 개점")
-        print("2 : 종료")
-        print("입력 : ",terminator: "")
-
+        Messages.menu.printMessage()
         if let choice = readLine(), let menu = Int(choice) {
             switch menu {
             case 1:
                 bank.open()
             case 2:
-                print("프로그램을 종료합니다.")
+                Messages.exit.printMessage()
                 return
             default:
-                print("올바른 메뉴를 선택해주세요.")
+                Messages.menuError.printMessage()
             }
         } else {
-            print("숫자를 입력해주세요.")
+            Messages.numberError.printMessage()
         }
     }
 }
