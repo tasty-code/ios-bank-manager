002d6e87f616f52747972c20605a584571bec196
yoruck2 <yoruck2@gmail.com>
2024-02-05 11:11:55 +0900

    feat: TaskType 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/TaskType.swift
@@ -0,0 +1,35 @@
+//
+//  WorkType.swift
+//  BankManagerConsoleApp
+//
+//  Created by dopamint on 2/1/24.
+//
+
+import Foundation
+
+enum TaskType: CaseIterable, CustomStringConvertible {
+    case loan
+    case deposit
+}
+
+extension TaskType {
+    
+    var description: String {
+        switch self {
+        case .loan:
+            return "대출"
+        case .deposit:
+            return "예금"
+        }
+    }
+    
+    var taskTime: Float {
+        switch self {
+        case .loan:
+            return 1.1
+        case .deposit:
+            return 0.7
+        }
+    }
+    
+}
