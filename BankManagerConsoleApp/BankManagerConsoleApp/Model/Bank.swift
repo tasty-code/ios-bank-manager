48fe657a823baba5daa622fcda181b222e78fd6a
Chan Ho Park <park_ch@kakao.com>
2024-02-05 15:06:11 +0900

    refactor: 고객 업무에 따른 다중처리 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -11,6 +11,9 @@ final class Bank<Q: QueueProtocol> where Q.Element == Customer {
     private var customerQueue: Q
     private let consoleMessage: ConsoleMessage
     private var totalCustomers: Int = 0
+    private let depositSemaphore = DispatchSemaphore(value: 2)
+    private let loanSemaphore = DispatchSemaphore(value: 1)
+    private var totalTime : Double = 0
     
     init(customerQueue: Q, consoleMessage: ConsoleMessage) {
         self.customerQueue = customerQueue
@@ -21,38 +24,69 @@ final class Bank<Q: QueueProtocol> where Q.Element == Customer {
     func open(completion: @escaping () -> Void) {
         let numberOfCustomers = Int.random(in: 10...30)
         for number in 1...numberOfCustomers {
-            customerQueue.enqueue(Customer(waitingNumber: number))
+            let task = Bool.random() ? TaskType.deposit : TaskType.loan
+            customerQueue.enqueue(Customer(waitingNumber: number, task: task))
         }
         totalCustomers = numberOfCustomers
-        processCustomers {
-            self.closed(completion: completion)
+        
+        processCustomer {
+            self.closed(totalTime: self.totalTime, completion: completion)
         }
     }
     
-    /// 고객 업무 처리
-    private func processCustomers(completion: @escaping () -> Void) {
-        let dispatchGroup = DispatchGroup()
+    /// 고객 업무 완료
+    func processCustomer(completion: @escaping () -> Void) {
+        let queue = DispatchQueue.global()
+        let group = DispatchGroup()
+        let startTime = DispatchTime.now()
         
-        while let customer = customerQueue.dequeue() {
-            dispatchGroup.enter()
-            consoleMessage.taskStartMessage(customerNumber: customer.waitingNumber)
+        for _ in 1...totalCustomers {
+            guard let customer = customerQueue.dequeue() else { continue }
             
-            DispatchQueue.global().asyncAfter(deadline: .now() + 0.7) {
-                self.consoleMessage.teskEndMessage(customerNumber: customer.waitingNumber)
-                dispatchGroup.leave()
+            switch customer.task {
+            case .deposit:
+                depositSemaphore.wait()
+                queue.async(group: group) {
+                    self.handleCustomer(customer)
+                    self.depositSemaphore.signal()
+                }
+            case .loan:
+                loanSemaphore.wait()
+                queue.async (group: group){
+                    self.handleCustomer(customer)
+                    self.loanSemaphore.signal()
+                }
             }
-            dispatchGroup.wait()
         }
-
-        dispatchGroup.notify(queue: .global()) {
-            completion()
+        
+        group.wait()
+        let endTime = DispatchTime.now()
+        totalTime = Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000
+        completion()
+        
+    }
+    
+    func handleCustomer(_ customer: Customer) {
+        switch customer.task {
+        case .deposit:
+            consoleMessage.taskStartMessage(customerNumber: customer.waitingNumber,
+                                            task: customer.task.description)
+            Thread.sleep(forTimeInterval: 0.7)
+            consoleMessage.teskEndMessage(customerNumber: customer.waitingNumber,
+                                          task: customer.task.description)
+        case .loan:
+            consoleMessage.taskStartMessage(customerNumber: customer.waitingNumber,
+                                            task: customer.task.description)
+            Thread.sleep(forTimeInterval: 1.1)
+            consoleMessage.teskEndMessage(customerNumber: customer.waitingNumber,
+                                          task: customer.task.description)
         }
     }
     
     /// 업무가 마감됨
-    private func closed(completion: @escaping () -> Void) {
-        let totalTime = Double(totalCustomers) * 0.7
-        consoleMessage.bankClosingMessage(totalCustomers: totalCustomers, time: totalTime)
+    func closed(totalTime: Double, completion: @escaping () -> Void) {
+//        consoleMessage.bankClosingMessage(totalCustomers: totalCustomers, time: totalTime)
+        consoleMessage.bankClosingMessage(totalCustomers: totalCustomers, time: totalTime )
         completion()
     }
 }
