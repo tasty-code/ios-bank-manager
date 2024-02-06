ac7b3ab1d0965320612acd5daea30f0ee525deb5
wlsdud-dev <hjymail1025@gmail.com>
2024-02-06 17:13:57 +0900

    refactor: BankClerk 구조체 대출 업무 메서드 추가, 메시지 객체 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
@@ -2,14 +2,21 @@
 //  BankClerk.swift
 //  BankManagerConsoleApp
 //
-//  Created by Jin-Mac on 1/30/24.
+//  Created by 루피, 진 on 1/30/24.
 //
 import Foundation
 
 struct BankClerk {
+    
     func work(for customer: Customer) {
-        print(BankMessage.start(customer.number).description)
+        print(BankMessage.start(customer.number, "예금").description)
         Thread.sleep(forTimeInterval: 0.7)
-        print(BankMessage.done(customer.number).description)
+        print(BankMessage.done(customer.number, "예금").description)
+    }
+    
+    func lendWorking(for customer: Customer) {
+        print(BankMessage.start(customer.number, "대출").description)
+        Thread.sleep(forTimeInterval: 1.1)
+        print(BankMessage.done(customer.number, "대출").description)
     }
 }
