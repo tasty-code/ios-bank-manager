848e8927d3f533580b1c5f0d0cf78b75c918483e
hemil0102 <hemil0102@gmail.com>
2024-02-06 16:35:08 +0900

    refactor: 컨벤션 수정 (#3)

--- BankManagerConsoleApp/Controller/CustomerManager.swift
+++ BankManagerConsoleApp/Controller/CustomerManager.swift
@@ -1,8 +1,8 @@
 
 struct CustomerManager {
     private(set) var customers: [Customer] = []
-    private(set) var loanTicketMachine = Queue<Customer>()
-    private(set) var depositTicketMachine = Queue<Customer>()
+    private(set) var loanTicketMachine: Queue<Customer> = Queue<Customer>()
+    private(set) var depositTicketMachine: Queue<Customer> = Queue<Customer>()
     
     mutating func createCustomers() {
         let number = Int.random(in: 10...30)
