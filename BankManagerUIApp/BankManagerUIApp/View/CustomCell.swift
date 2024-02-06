ae48f1b464457716b312ace57be2ac728c463d1d
NERO <mxzxro@gmail.com>
2024-02-06 19:06:56 +0900

    refactor: UITableViewCell 로 변경(UILabel -> UITableViewCell)

---
+++ BankManagerUIApp/BankManagerUIApp/View/CustomCell.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    private var label:  UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    func setLabelText(customerNumber: Int, serviceType: String) {
+        self.label.text = "\(customerNumber) - \(serviceType)"
+    }
+    
+    func setLabelColor(_ color: UIColor) {
+        self.label.textColor = color
+    }
+}
