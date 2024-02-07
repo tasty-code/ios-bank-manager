298b933086ac682eeea93a812a407391db8e396a
eunjuchoi <eunjuzzoo@naver.com>
2024-02-07 19:07:30 +0900

    chore: Customer 구조체 initializer 명시적으로 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Constants/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Constants/Customer.swift
@@ -10,4 +10,9 @@ import Foundation
 struct Customer {
     let number: Int
     let service: BankService
+    
+    init(number: Int, service: BankService) {
+        self.number = number
+        self.service = service
+    }
 }
