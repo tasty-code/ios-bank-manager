8273f4be34fc04ffde72494014243a3896653af9
suojae3 <suojae3@gmail.com>
2024-02-07 17:39:11 +0900

    fix: 2번 입력시 종료 안되던 오류 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -14,6 +14,7 @@ final class Bank {
     private let startNumber: Int = 1
     private let userChoiceRange: ClosedRange<Int> = 1...2
     private let customerCountRange: ClosedRange<Int> = 10...30
+    private var exitInput = false
     
     init(bankManager: BankManager) {
         self.bankManager = bankManager
@@ -22,30 +23,32 @@ final class Bank {
     func open() async {
         print(Message.default.showMessage())
         print(Message.userInput.showMessage(), terminator: "")
-        let userInput = readLine()
+        guard let userInput = readLine() else { return }
         
         do {
             try validateUserInput(with: userInput)
+            customers = MemberFactory.makeCustomers(count: Int.random(in: customerCountRange))
             await startTask()
         } catch {
             print(Message.inputError.showMessage())
             await open()
         }
     }
-    
-    private func validateUserInput(with userInput: String?) throws {
-        guard let userInput,
-              let userChoice = Int(userInput),
-              userChoiceRange.contains(userChoice)
-        else {
+            
+    private func validateUserInput(with userInput: String) throws {
+        let userChoice = Int(userInput)
+        switch userChoice {
+        case 1:
+            break
+        case 2:
+            exitInput = true
+        default:
             throw BankError.inputError
         }
-
-        guard userChoice == startNumber else { return }
-        customers = MemberFactory.makeCustomers(count: Int.random(in: customerCountRange))
     }
             
     private func startTask() async {
+        guard exitInput == false else { return }
         await alignCustomer(with: customers)
         await handleTask()
         await open()
