de41264261411ab18832e78a42211b7fd3695769
eunjuchoi <eunjuzzoo@naver.com>
2024-02-07 19:02:04 +0900

    chore: BankService 타입 파일 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Constants/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Constants/Customer.swift
@@ -7,21 +7,6 @@
 
 import Foundation
 
-enum BankService: String, CaseIterable {
-
-    case loan = "대출"
-    case deposit = "예금"
-    
-    var requiredTime: Double {
-        switch self {
-        case .loan:
-            return 1.1
-        case .deposit:
-            return 0.7
-        }
-    }
-}
-
 struct Customer {
     let number: Int
     let service: BankService
