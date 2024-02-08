f614f299ae6a0cbb83b3b6bf4bdbec58a217bfe8
eunjuchoi <eunjuzzoo@naver.com>
2024-02-08 16:18:10 +0900

    feat: 의존성 주입 코드 추가

--- BankManagerUIApp/BankManagerUIApp/App/SceneDelegate.swift
+++ BankManagerUIApp/BankManagerUIApp/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = BankViewController()
+        window?.rootViewController = BankViewController(viewModel: BankViewModel())
         window?.makeKeyAndVisible()
     }
 
