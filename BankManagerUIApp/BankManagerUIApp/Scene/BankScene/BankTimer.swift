f6e4a0e6e689a490aef2a33133f2f593e432c0f1
suojae3 <suojae3@gmail.com>
2024-02-08 16:32:46 +0900

    chore: BankTimer 네이밍 변경

--- BankManagerUIApp/BankManagerUIApp/Scene/BankScene/BankTimer.swift
+++ BankManagerUIApp/BankManagerUIApp/Scene/BankScene/BankTimer.swift
@@ -13,26 +13,26 @@ private struct TimeUnit {
     let seconds: Int
 }
 
-final class TimerHandler {
+final class BankTimer {
     
     private var timer = Timer()
     private var count = 0
-    private(set) var timeString = ""
-
-    func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.00001, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+    var getTimeString: ((String) -> Void)?
+    
+    func start() {
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
     }
     
-    func stopTimer() {
+    func stop() {
         timer.invalidate()
-        timeString = "00:00:00"
+        getTimeString?("00:00:00")
     }
     
     @objc private func timerCounter() {
         count += 1
         let time = secondsToHoursMinutesSeconds(seconds: count)
-        let timeString = makeTimeString(time)
-        self.timeString = timeString
+        var convertedTimeString = makeTimeString(time)
+        getTimeString?(convertedTimeString)
     }
     
     private func secondsToHoursMinutesSeconds(seconds: Int) -> TimeUnit {
