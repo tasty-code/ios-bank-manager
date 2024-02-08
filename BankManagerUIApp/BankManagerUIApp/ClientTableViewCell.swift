673aa2410259b5500ae5b59fb70a8bafbce7fbab
eunjuchoi <eunjuzzoo@naver.com>
2024-02-08 09:46:26 +0900

    feat: 커스텀 tableViewCell 정의

---
+++ BankManagerUIApp/BankManagerUIApp/ClientTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  ClientTableViewCell.swift
+//  BankManagerUIApp
+//
+//  Created by EUNJU on 2024/02/07.
+//
+
+import UIKit
+
+final class ClientTableViewCell: UITableViewCell {
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 20)
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        selectionStyle = .none
+        
+        contentView.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    func setUpData(data: Client) {
+        titleLabel.text = "\(data.number) - \(data.bankTask.description)"
+    }
+}
