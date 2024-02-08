ac2b203e40445a38cc5713323088c7156d39c231
mireu <148876644+mireu930@users.noreply.github.com>
2024-02-08 18:04:41 +0900

    refactor: 파일분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct Bank {
-    private let bankManager: BankManager
-    private let customNum: Int
-    
-    init(customNum: Int, bankManager: BankManager) {
-            self.customNum = customNum
-            self.bankManager = bankManager
-        }
-    
+//struct Bank {
+//    private let bankManager: BankManager
+//    private let customNum: Int
+//    
+//    init(customNum: Int, bankManager: BankManager) {
+//            self.customNum = customNum
+//            self.bankManager = bankManager
+//        }
+//    
 //    func openBank() {
 //        setCustomerCount(customer: customNum)
 //        let workTime = calulateWorkTime {
@@ -27,19 +27,19 @@ struct Bank {
 //        }
 //        closeBank(time: workTime)
 //    }
-    
-    private func setCustomerCount(customer: Int) {
-        (1...customer).forEach { bankManager.standBy(customer: Customer(banking: Banking.allCases.randomElement(), numOfPerson: $0)) }
-    }
-    
-    private func calulateWorkTime(work: () -> Void) -> String {
-        let startTime = CFAbsoluteTimeGetCurrent()
-        work()
-        let endTime = CFAbsoluteTimeGetCurrent()
-        return String(format: "%.2f", endTime - startTime)
-    }
-    
-    private func closeBank(time: String) {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customNum)명이며, 총 업무시간은 \(time)초 입니다.")
-    }
-}
+//    
+//    private func setCustomerCount(customer: Int) {
+//        (1...customer).forEach { bankManager.standBy(customer: Customer(banking: Banking.allCases.randomElement(), numOfPerson: $0)) }
+//    }
+//    
+//    private func calulateWorkTime(work: () -> Void) -> String {
+//        let startTime = CFAbsoluteTimeGetCurrent()
+//        work()
+//        let endTime = CFAbsoluteTimeGetCurrent()
+//        return String(format: "%.2f", endTime - startTime)
+//    }
+//    
+//    private func closeBank(time: String) {
+//        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customNum)명이며, 총 업무시간은 \(time)초 입니다.")
+//    }
+//}
--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankClerk.swift
@@ -7,28 +7,28 @@
 
 import Foundation
 
-struct BankClerk {
-    private var work: Banking
-    private var pace: Double {
-        switch work {
-        case .deposit:
-           return 0.7
-        case .loan:
-           return 1.1
-        }
-    }
-    
-    init(work: Banking) {
-        self.work = work
-    }
-    
-    func recieve(customer: Customer) {
-        print("\(customer.numOfPerson)번 고객 \(work.rawValue)업무 시작")
-        paceTime(pace)
-        print("\(customer.numOfPerson)번 고객 \(work.rawValue)업무 완료")
-    }
-    
-    private func paceTime(_ pace: Double) {
-        usleep(useconds_t(pace * 1000000))
-    }
-}
+//struct BankClerk {
+//    private var work: Banking
+//    private var pace: Double {
+//        switch work {
+//        case .deposit:
+//           return 0.7
+//        case .loan:
+//           return 1.1
+//        }
+//    }
+//    
+//    init(work: Banking) {
+//        self.work = work
+//    }
+//    
+//    func recieve(customer: Customer) {
+//        print("\(customer.numOfPerson)번 고객 \(work.rawValue)업무 시작")
+//        paceTime(pace)
+//        print("\(customer.numOfPerson)번 고객 \(work.rawValue)업무 완료")
+//    }
+//    
+//    private func paceTime(_ pace: Double) {
+//        usleep(useconds_t(pace * 1000000))
+//    }
+//}
