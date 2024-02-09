41c2dd3a7b273d07a858011c514da3b6eb678dfb
eunjuchoi <eunjuzzoo@naver.com>
2024-02-09 12:08:46 +0900

    feat: 은행 업무 처리 기능 구현

--- BankManagerUIApp/BankManagerUIApp/Scene/BankScene/BankViewModel.swift
+++ BankManagerUIApp/BankManagerUIApp/Scene/BankScene/BankViewModel.swift
@@ -8,16 +8,23 @@
 import Foundation
 
 final class BankViewModel {
-
+    
+    // MARK: Properties
     private let repository = BankRepository()
     private let timer = BankTimer()
     
     private(set) var waitingClients: Observable<[Client]> = Observable([])
+    private(set) var workingClients: Observable<[Client]> = Observable([])
     private(set) var timeString: Observable<String> = Observable("")
-
+    private let loanSemaphore = DispatchSemaphore(value: 1)
+    private let depositSemaphore = DispatchSemaphore(value: 2)
+    
+    // MARK: Custom Methods
     func fetchData() {
         repository.makeClientData { [weak self] result in
             self?.waitingClients.value += result
+            self?.startTask()
+            self?.start()
         }
     }
     
@@ -34,5 +41,25 @@ final class BankViewModel {
     
     func stop() {
         timer.stop()
+    }
+    func startTask() {
+        while !waitingClients.value.isEmpty {
+            let client = waitingClients.value.removeFirst()
+            workingClients.value.append(client)
+            handleTask(of: client)
+        }
+    }
+    
+    private func handleTask(of client: Client) {
+        let semaphore = client.bankTask == .loan ? loanSemaphore : depositSemaphore
+    
+        semaphore.wait()
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else {return}
+            Thread.sleep(forTimeInterval: client.bankTask.requiredTime)
+            if self.workingClients.value.isEmpty { return }
+            self.workingClients.value.removeFirst()
+            semaphore.signal()
+        }
     }
 }
