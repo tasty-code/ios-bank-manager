3814d579386d52ef503207f5f707b332638176b6
yoruck2 <yoruck2@gmail.com>
2024-02-10 15:22:54 +0900

    refactor: 4개의 큐 -> 1개의 손님큐 , DispatchSemaphore 활용

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -8,77 +8,52 @@
 import Foundation
 
 final class Bank {
-    private let depositBankerCount: Int
-    private let loanBankerCount: Int
-    private var totalDepositWorkingTime: Double
-    private var totalLoanWorkingTime: Double
-    
-    private var depositBankerQueue = Queue<Banker>()
-    private var loanBankerQueue = Queue<Banker>()
-    private var depositCustomerQueue = Queue<Customer>()
-    private var loanCustomerQueue = Queue<Customer>()
+
+    private var banker = Banker()
+    private var customerQueue = Queue<Customer>()
+    private let depositSemaphore: DispatchSemaphore
+    private let loanSemaphore: DispatchSemaphore
     private let group = DispatchGroup()
     
     init(depositBankerCount: Int, loanBankerCount: Int) {
-        self.depositBankerCount = depositBankerCount
-        self.loanBankerCount = loanBankerCount
-        self.totalDepositWorkingTime = 0.0
-        self.totalLoanWorkingTime = 0.0
+        self.depositSemaphore = DispatchSemaphore(value: depositBankerCount)
+        self.loanSemaphore = DispatchSemaphore(value: loanBankerCount)
     }
     
     func open() {
+        let startTime = Date()
         Messages.openBank.printMessage()
         let numberOfCustomers = Int.random(in: 10...30)
-
+        
         print("고객 수: \(numberOfCustomers)")
-        setUpBankerQueue(depositBankerCount: depositBankerCount, loanBankerCount: loanBankerCount)
         setUpCustomerQueue(count: numberOfCustomers)
         
-        while !depositCustomerQueue.isEmpty() || !loanCustomerQueue.isEmpty() {
-            serveCustomer(bankerQueue: depositBankerQueue, customerQueue: depositCustomerQueue)
-            serveCustomer(bankerQueue: loanBankerQueue, customerQueue: loanCustomerQueue)
+        while let customer = customerQueue.dequeue() {
+            switch customer.taskType {
+            case .deposit:
+                serveCustomer(semaphore: depositSemaphore, customer: customer)
+            case .loan:
+                serveCustomer(semaphore: loanSemaphore, customer: customer)
+            }
         }
         group.wait()
-        
-        let formattedTotalWorkingTime = max(totalLoanWorkingTime, totalDepositWorkingTime).formattedDecimal
-        Messages.closeBank(customerCount: numberOfCustomers, totalTime: formattedTotalWorkingTime).printMessage()
-        depositBankerQueue.clear()
-        loanBankerQueue.clear()
+        let endTime = Date()
+        let elapsedTime = endTime.timeIntervalSince(startTime).formattedDecimal
+        Messages.closeBank(customerCount: numberOfCustomers, totalTime: elapsedTime).printMessage()
     }
     
-    private func setUpBankerQueue(depositBankerCount: Int, loanBankerCount: Int) {
-        for _ in 1...depositBankerCount {
-            depositBankerQueue.enqueue(Banker(taskType: .deposit))
-        }
-        for _ in 1...loanBankerCount {
-            loanBankerQueue.enqueue(Banker(taskType: .loan))
+    private func serveCustomer(semaphore: DispatchSemaphore, customer: Customer) {
+        DispatchQueue.global().async(group: group) {
+            semaphore.wait()
+            self.banker.processCustomer(customer)
+            semaphore.signal()
         }
     }
     
     private func setUpCustomerQueue(count: Int) {
         for number in 1...count {
-            guard let customer = Customer(number: number) else { return }
-            if customer.taskType == .deposit {
-                depositCustomerQueue.enqueue(customer)
-            } else if customer.taskType == .loan {
-                loanCustomerQueue.enqueue(customer)
-            }
-        }
-    }
-    
-    private func processTask(banker: Banker, customer: Customer, bankerQueue: Queue<Banker>) {
-        DispatchQueue.global().async(group: group)  { [self] in
-            totalDepositWorkingTime += banker.processCustomer(customer)
-            bankerQueue.enqueue(Banker(taskType: banker.taskType))
-        }
-    }
-    
-    private func serveCustomer(bankerQueue: Queue<Banker>, customerQueue: Queue<Customer>) {
-        if bankerQueue.isEmpty() == false {
-            guard let customer = customerQueue.dequeue(),
-                  let banker = bankerQueue.dequeue()
-            else { return }
-            processTask(banker: banker, customer: customer, bankerQueue: bankerQueue)
+            let customer = Customer(number: number)
+            customerQueue.enqueue(customer)
         }
     }
 }
