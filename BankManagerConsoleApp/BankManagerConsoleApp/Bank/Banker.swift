2bef8df5b730309c54e8c1d760938dee0a0457bf
yoruck2 <yoruck2@gmail.com>
2024-02-10 15:16:29 +0900

    refactor: Banker타입 간소화

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Banker.swift
@@ -8,18 +8,10 @@
 import Foundation
 
 final class Banker {
-    let taskType: TaskType
-    var duration: Double
-    
-    init(taskType: TaskType) {
-        self.taskType = taskType
-        self.duration = taskType.taskTime
-    }
-    
-    func processCustomer(_ customer: Customer) -> Double {
-            Messages.taskStart(number: customer.number, taskType: taskType.description).printMessage()
-            Thread.sleep(forTimeInterval: duration)
-            Messages.taskDone(number: customer.number, taskType: taskType.description).printMessage()
-            return duration
+
+    func processCustomer(_ customer: Customer) {
+        Messages.taskStart(number: customer.number, taskType: customer.taskType.description).printMessage()
+        Thread.sleep(forTimeInterval: customer.taskType.taskTime)
+        Messages.taskDone(number: customer.number, taskType: customer.taskType.description).printMessage()
     }
 }
