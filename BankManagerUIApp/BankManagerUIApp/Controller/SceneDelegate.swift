628697e3268ce5fbd69f98f8c4a3c472386428f6
eensungkim <eensungkim@gmail.com>
2024-02-10 10:35:02 +0900

    chore: 네이밍 변경

--- BankManagerUIApp/BankManagerUIApp/Controller/SceneDelegate.swift
+++ BankManagerUIApp/BankManagerUIApp/Controller/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = MainViewController(dataSource: BankManager())
+        window?.rootViewController = MainViewController(manager: BankManager())
         window?.makeKeyAndVisible()
     }
 }
